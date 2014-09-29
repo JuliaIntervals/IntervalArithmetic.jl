4f4561bea9c2686f4b22325ea420298f8b2cb83a
David P. Sanders <dpsanders@gmail.com>
2014-09-29 18:23:22 -0500

    Removed unneeded lines from runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,20 +1,12 @@
-# Simple tests for @intervals
-#using @intervals
+# Tests for ValidatedNumerics
 using ValidatedNumerics
 using Base.Test
 
-set_bigfloat_precision(53)
-
-a = @interval(1.1, 0.1)
-b = @interval(0.9, 2.0)
-c = @interval(0.25, 4.0)
+# Tests for Intervals.jl:
 
 include("consistency_tests.jl")
 include("numeric_tests.jl")
 include("trig_tests.jl")
-
-
-
 
 
 println("    \033[32;1mSUCCESS\033[0m")
