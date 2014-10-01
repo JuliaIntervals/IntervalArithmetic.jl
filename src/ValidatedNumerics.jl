2a03eb47b4a8b7efc1b3de599d3a2c462e409130
David P. Sanders <dpsanders@gmail.com>
2014-10-01 11:12:16 -0500

    Moved power, exp and log to interval_functions.jl

--- src/ValidatedNumerics.jl
+++ src/ValidatedNumerics.jl
@@ -1,7 +1,13 @@
 module ValidatedNumerics
 
-# package code goes here
+# Default precision:
+set_bigfloat_precision(53)
+
+
 
 include("Intervals.jl")
+
+include("interval_trig.jl")
+include("interval_functions.jl")
 
 end # module
