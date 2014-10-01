45e8b8fc8ce850b8827477cdcdc8f57b4a7c34f5
David P. Sanders <dpsanders@gmail.com>
2014-10-01 11:23:50 -0500

    Moved arithmetic to interval_arithmetic.jl. Moved Intervals.jl to interval_definition.jl

--- src/ValidatedNumerics.jl
+++ src/ValidatedNumerics.jl
@@ -1,6 +1,20 @@
 module ValidatedNumerics
 
-# Default precision:
+
+import Base:
+  in, zero, one, abs, real, show,
+  sqrt, exp, log, sin, cos, tan, inv,
+  union, intersect, isempty,
+  convert, promote_rule,
+  BigFloat, string
+
+export
+  @interval, Interval
+  @round_down, @round_up, @round, @thin_interval,
+  diam, mid, mag, mig, hull, isinside
+
+
+## Default precision:
 set_bigfloat_precision(53)
 
 
@@ -11,10 +25,12 @@ BigFloat(a::MathConst) = big(a)
 <(a::MathConst, b::MathConst) = big(a) < big(b)
 
 
-include("Intervals.jl")
+## Includes:
 
+include("interval_definition.jl")
 include("interval_macros.jl")
+include("interval_arithmetic.jl")
 include("interval_trig.jl")
 include("interval_functions.jl")
 
-end # module
+end # module ValidatedNumerics
