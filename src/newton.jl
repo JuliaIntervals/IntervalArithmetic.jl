9dde6e1757dfb830add5178d03096164f164618f
David P. Sanders <dpsanders@gmail.com>
2014-10-05 13:49:52 -0500

    Cleaned up newton.jl

--- src/newton.jl
+++ src/newton.jl
@@ -4,9 +4,16 @@ using AutoDiff
 isthin(x) = (m = (x.lo + x.hi) / 2; m == x.lo || m == x.hi)  # no more precision
 
 
-#mid(x::Interval) = @interval((x.lo + x.hi) / 2)
+function guarded_mid(x)
+    m = (x.lo + x.hi) / 2
+    if m == 0  # midpoint exactly 0
+        alpha = 0.45
+        m = alpha*x.lo + (1-alpha)*x.hi   # displace to another point in interval
+    end
 
-#mid(x::Interval) = Interval(@round_down( (x.lo + x.hi) / 2 ), @round_up( (x.lo + x.hi) / 2) )
+    m
+
+end
 
 function N(f::Function, x::Interval, deriv=None)
 
@@ -14,22 +21,8 @@ function N(f::Function, x::Interval, deriv=None)
         deriv = differentiate(f, x)
     end
 
-#     set_rounding(BigFloat, RoundDown)
-#     m1 = (x.lo + x.hi) / 2
-#     set_rounding(BigFloat, RoundUp)
-#     m2 = (x.lo + x.hi) / 2
-
-#     m = Interval(m1, m2)  # rounded mid-point
-#     @show m
-
-    m = (x.lo + x.hi) / 2
-    if m == 0  # midpoint exactly 0
-        alpha = 0.45
-        m = alpha*x.lo + (1-alpha)*x.hi   # displace to another point in interval
-    end
-
+    m = guarded_mid(x)
     m = Interval(m)
-
 
     Nx = m - ( f(m) / deriv )
 
@@ -37,15 +30,15 @@ end
 
 
 function refine(f::Function, x::Interval)
-    println("Refining $x")
+    #println("Refining $x")
     i = 0
     while i < 20  # avoid problem with tiny floating-point numbers if 0 is a root
         Nx = N(f, x)
         Nx = Nx ∩ x
-        @show x, Nx
+        #@show x, Nx
         # if Nx == x
         if Nx == x
-            return @show (x, :unique)
+            return (x, :unique)
         end
         x = Nx
         i += 1
@@ -56,27 +49,28 @@ end
 
 newton(f::Function, x::Nothing) = []
 
-# *strict* subset:
+# subset:
+⊆(a::Interval, b::Interval) = b.lo <= a.lo && a.hi <= b.hi
+#⊆(a::Nothing, b::Interval) = false
+
+# strict subset:
 ⊂(a::Interval, b::Interval) = b.lo < a.lo && a.hi < b.hi
-⊂(a::Nothing, b::Interval) = false
-
-
+#⊂(a::Nothing, b::Interval) = false
 
 function newton(f::Function, x::Interval)
 
     if isempty(x)
         return []
     end
-    # @show x
 
-    m = mid(x)
+
     deriv = differentiate(f, x)
 
     if !(0 in deriv)
         Nx = N(f, x, deriv)
 
         println("Inside newton")
-        @show (x, Nx)
+        #@show (x, Nx)
 
         #if Nx ⊂ x
         if Nx in x && diam(Nx) < diam(x)
@@ -92,18 +86,22 @@ function newton(f::Function, x::Interval)
         end
 
         # bisect:
+
+        println("Bisecting...")
+
+        m = mid(x)
         return vcat(newton(f, Interval(x.lo, m)),  # must be careful with rounding of m ?
                     newton(f, Interval(m, x.hi))
                     )
 
-    else  # 0 in deriv
+    else  # 0 in deriv; this does extended interval division by hand
         y1 = Interval(deriv.lo, -0.0)
         y2 = Interval(0.0, deriv.hi)
 
         y1 = N(f, x, y1) ∩ x
         y2 = N(f, x, y2) ∩ x
 
-        @show (y1, y2)
+        #@show (y1, y2)
 
 
         return vcat(newton(f, y1),
