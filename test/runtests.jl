e060df6dd04bb06c9ba59aa4449678acd71417f1
David P. Sanders <dpsanders@gmail.com>
2014-10-06 23:42:24 -0500

    root_finding_tests.jl has a first stab at solving the Wilkinson polynomial, currently failing

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,6 +6,7 @@ using Base.Test
 # Tests for Intervals.jl:
 
 include("interval_tests/interval_tests.jl")
+include("root_finding_tests/root_finding_tests.jl")
 
 
 println("    \033[32;1mSUCCESS\033[0m")
