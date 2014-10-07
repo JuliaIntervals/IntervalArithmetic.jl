8f86241e15bfa92f3b5110825c0b5e0e69d938e0
David P. Sanders <dpsanders@gmail.com>
2014-10-07 00:17:36 -0500

    Cleaned up Wilkinson test

--- test/root_finding_tests/root_finding_tests.jl
+++ test/root_finding_tests/root_finding_tests.jl
@@ -2,23 +2,11 @@ using ValidatedNumerics
 using Polynomials
 
 
-# w = poly(collect(1:20))  # Wilkinson polynomial
-# coeffs = map(BigFloat, w.a)
-
-# wilkinson(x) = sum( [coeffs[i]*x^(i-1) for i in 1:21] )
-
-# #wilkinson(x) = polyval(w, x)
-
-# a = @interval(-1, 21)
-
-# #@time newton_roots = newton(wilkinson, a)
-# @time krawczyk_roots = krawczyk(wilkinson, a)
-
-
-# try with smaller polynomials:
+# Wilkinson-type polynomial defined by its roots:
+# wₙ(x) = (x-1)⋅(x-2)⋅ ⋯ ⋅ (x-n)
 
 function wilkinson_type(n)
-    p = poly(collect(1:n))  # Wilkinson-type polynomial
+    p = poly(collect(1:n))
     coeffs = map(BigFloat, p.a)
 
     f(x) = sum( [coeffs[i]*x^(i-1) for i in 1:length(coeffs)] )
