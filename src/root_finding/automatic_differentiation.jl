fb88beb555652c47039950813f20b7a5eb285b9f
David P. Sanders <dpsanders@gmail.com>
2014-10-08 17:09:02 -0500

    Added conversion and promotion to Jet. Made Jet a subtype of Number to enable this.

--- src/root_finding/automatic_differentiation.jl
+++ src/root_finding/automatic_differentiation.jl
@@ -3,13 +3,18 @@
 
 ## Represents the jet of a function u at the point a by (u(a), u'(a))
 
-type Jet
+immutable Jet <: Number  # is this really a Number?  Then promotion rules work
     val   # value u(a)
-    der # derative u'(a)
+    der   # derative u'(a)
 end
 
+import Base.convert, Base.promote_rule
+
+convert(::Type{Jet}, c::Real) = Jet(c)
+promote_rule{T <: Real}(::Type{Jet}, ::Type{T}) = Jet
+
 # Constants:
-Jet(c) = Jet(c, 0)
+Jet(c::Real) = Jet(c, 0)
 
 # Arithmetic between two Jet
 +(x::Jet, y::Jet) = Jet(x.val + y.val, x.der + y.der)
@@ -27,12 +32,15 @@ end
 # Arithmetic operations between Jet and intervals/numbers
 # This may be able to be replaced by suitable promotion and convert statements
 
-for op in (:+, :-, :*, :/)
-    @eval begin
-        $op(u::Jet, c::Real) = $op(u, Jet(c))
-        $op(c::Real, u::Jet) = $op(Jet(c), u)
-    end
-end
+# for op in (:+, :-, :*, :/)
+#     @eval begin
+#         $op(u::Jet, c::Real) = $op(u, Jet(c))
+#         $op(c::Real, u::Jet) = $op(Jet(c), u)
+#     end
+# end
+
+
+#promote_rule(::Type{BigFloat}, ::Type{Interval}) = Interval
 
 +(x::Jet) = x
 -(x::Jet) = Jet(-x.val, -x.der)
@@ -47,6 +55,7 @@ exp(x::Jet) = Jet(exp(x.val), x.der * exp(x.val))
 log(x::Jet) = Jet(log(x.val), x.der / x.val)
 
 ^(x::Jet, n::Integer) = n==0 ? Jet(0, 0) : Jet( (x.val)^n, n * (x.val)^(n-1) * x.der )
+^(x::Jet, r::Rational) = x^(r.num)^(1/r.den)
 ^(x::Jet, y::Real) = Jet( (x.val)^y, y * (x.val)^(y-1) * x.der )
 
 differentiate(f, a) = f( Jet(a, 1) ).der
