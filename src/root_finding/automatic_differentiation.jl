c612c426731391a0c9361af67ad56f3f9b7df85f
David P. Sanders <dpsanders@gmail.com>
2014-10-08 11:21:47 -0500

    Renamed value to val and deriv to der in auto_diff.jl

--- src/root_finding/automatic_differentiation.jl
+++ src/root_finding/automatic_differentiation.jl
@@ -4,23 +4,23 @@
 ## Represents the jet of a function u at the point a by (u(a), u'(a))
 
 type Jet
-    value
-    deriv
+    val   # value u(a)
+    der # derative u'(a)
 end
 
 # Constants:
 Jet(c) = Jet(c, 0)
 
 # Arithmetic between two Jet
-+(x::Jet, y::Jet) = Jet(x.value + y.value, x.deriv + y.deriv)
--(x::Jet, y::Jet) = Jet(x.value - y.value, x.deriv - y.deriv)
-*(x::Jet, y::Jet) = Jet(x.value*y.value, x.value*y.deriv + y.value*x.deriv)
++(x::Jet, y::Jet) = Jet(x.val + y.val, x.der + y.der)
+-(x::Jet, y::Jet) = Jet(x.val - y.val, x.der - y.der)
+*(x::Jet, y::Jet) = Jet(x.val*y.val, x.val*y.der + y.val*x.der)
 
 function /(x::Jet, y::Jet)
-    quotient = x.value / y.value
-    deriv = (x.deriv - quotient*y.deriv) / y.value
+    quotient = x.val / y.val
+    der = (x.der - quotient*y.der) / y.val
 
-    Jet(quotient, deriv)
+    Jet(quotient, der)
 end
 
 
@@ -35,21 +35,21 @@ for op in (:+, :-, :*, :/)
 end
 
 +(x::Jet) = x
--(x::Jet) = Jet(-x.value, -x.deriv)
+-(x::Jet) = Jet(-x.val, -x.der)
 
 
 # Elementary functions
 
-sin(x::Jet) = Jet(sin(x.value), x.deriv*cos(x.value))
-cos(x::Jet) = Jet(cos(x.value), -x.deriv*sin(x.value))
+sin(x::Jet) = Jet(sin(x.val), x.der*cos(x.val))
+cos(x::Jet) = Jet(cos(x.val), -x.der*sin(x.val))
 
-exp(x::Jet) = Jet(exp(x.value), x.deriv * exp(x.value))
-log(x::Jet) = Jet(log(x.value), x.deriv / x.value)
+exp(x::Jet) = Jet(exp(x.val), x.der * exp(x.val))
+log(x::Jet) = Jet(log(x.val), x.der / x.val)
 
-^(x::Jet, n::Integer) = n==0 ? Jet(0, 0) : Jet( (x.value)^n, n * (x.value)^(n-1) * x.deriv )
-^(x::Jet, y::Real) = Jet( (x.value)^y, y * (x.value)^(y-1) * x.deriv )
+^(x::Jet, n::Integer) = n==0 ? Jet(0, 0) : Jet( (x.val)^n, n * (x.val)^(n-1) * x.der )
+^(x::Jet, y::Real) = Jet( (x.val)^y, y * (x.val)^(y-1) * x.der )
 
-differentiate(f, a) = f(Jet(a, 1.)).deriv
+differentiate(f, a) = f( Jet(a, 1) ).der
 const D = differentiate
 
 function jacobian(f, a)
