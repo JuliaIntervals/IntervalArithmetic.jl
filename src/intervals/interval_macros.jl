e7cd836321363d264611e14b3d163023b5215bfc
David P. Sanders <dpsanders@gmail.com>
2014-10-08 20:58:09 -0500

    Added function floatinterval to create and manipulate intervals of floats. floatinterval accepts a BigFloat interval created with @interval and returns a correctly-rounded interval of Floats. To do this in a simple way, I added changing both Float and BigFloat rounding modes in the @round_down and @round_up macros.

--- src/intervals/interval_macros.jl
+++ src/intervals/interval_macros.jl
@@ -3,21 +3,37 @@
 
 # (Could use "with rounding" to ensure previous rounding mode is correctly reset)
 
+evaluate(expr) = eval(Main, expr)
+
 macro round_down(expr)
-    quote
-        #set_rounding(BigFloat, RoundDown)
-        with_rounding(BigFloat, RoundDown) do
-            $expr
+#     value = evaluate(expr)
+
+#     if typeof(value) == Float64
+#         quote
+#             set_rounding(Float64, RoundDown)
+#             $expr
+#         end
+
+#     else
+
+        quote
+            #set_rounding(BigFloat, RoundDown)
+            #with_rounding(BigFloat, RoundDown) do
+            set_rounding(BigFloat, RoundDown)
+            set_rounding(Float64, RoundDown)
+                $expr
+           # end
         end
-    end
+#     end
 end
 
 macro round_up(expr)
     quote
-        #set_rounding(BigFloat, RoundUp)
-         with_rounding(BigFloat, RoundUp) do
+        set_rounding(BigFloat, RoundUp)
+        set_rounding(Float64, RoundUp)
+         #with_rounding(BigFloat, RoundUp) do
             $expr
-        end
+        #end
     end
 end
 
@@ -33,9 +49,10 @@ end
 
 
 
-transform(a::MathConst) = ( @thin_interval(big(a)))
-transform(a::BigFloat) = ( @thin_interval(a))
-transform(a::Number) = :( @thin_interval(BigFloat(string($a))) )
+transform(a::MathConst) =  ( @thin_interval(big(a)))
+transform(a::BigFloat)  =  ( @thin_interval(a))
+transform(a::Number)    = :( @thin_interval(BigFloat(string($a))) )
+
 
 function transform(a::Symbol)
 
@@ -84,9 +101,41 @@ end
 
 
 
-
 macro round(expr1, expr2)
     quote
         Interval(@round_down($expr1), @round_up($expr2))
     end
+end
+
+
+# macro floatinterval(expr1, expr2)
+
+#     expr3 = @interval($expr1, $expr2)
+#     I = eval(expr3)
+
+#     quote
+#         set_rounding(Float64, RoundDown)
+#         f1 = float(I.lo)
+
+#         set_rounding(Float64, RoundUp)
+#         f2 = float(I.hi)
+
+#         Interval(f1, f2)
+
+#     end
+
+# end
+
+
+function round_BigFloat_to_float(x, rounding_mode::RoundingMode)
+     with_rounding(BigFloat, rounding_mode) do
+        convert(Float64, x)
+    end
+end
+
+function floatinterval(x::Interval)
+    lo = round_BigFloat_to_float(x.lo, RoundDown)
+    hi = round_BigFloat_to_float(x.hi, RoundUp)
+
+    Interval(lo, hi)
 end
