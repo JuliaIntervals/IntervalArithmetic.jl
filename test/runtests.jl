55831e86ac92898074139da89e53d9f1497da087
David P. Sanders <dpsanders@gmail.com>
2014-10-11 09:53:09 -0500

    Remove root_finding_tests from this branch

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,8 +6,8 @@ using Base.Test
 # Tests for Intervals.jl:
 
 include("interval_tests/interval_tests.jl")
-include("root_finding_tests/auto_diff_tests.jl")
-include("root_finding_tests/root_finding_tests.jl")
+#include("root_finding_tests/auto_diff_tests.jl")
+#include("root_finding_tests/root_finding_tests.jl")
 
 
 println("    \033[32;1mSUCCESS\033[0m")
