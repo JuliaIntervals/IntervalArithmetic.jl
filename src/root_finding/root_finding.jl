5e5822bc08efd0e0293e6838f4ec2faae4e7390d
David P. Sanders <dpsanders@gmail.com>
2014-11-01 15:14:23 -0600

    Made find_roots methods more specific to avoid ambiguity

--- src/root_finding/root_finding.jl
+++ src/root_finding/root_finding.jl
@@ -4,11 +4,11 @@ include("newton.jl")
 include("krawczyk.jl")
 
 
-function find_roots(f::Function, a::Interval, method=newton)
+function find_roots(f::Function, a::Interval, method::Function=newton)
     method(f, a)
 end
 
-function find_roots(f::Function, f_prime::Function, a::Interval, method=newton)
+function find_roots(f::Function, f_prime::Function, a::Interval, method::Function=newton)
     method(f, f_prime, a)
 end
 
