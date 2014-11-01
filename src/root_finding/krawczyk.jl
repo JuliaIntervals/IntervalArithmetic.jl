d89af5d5b44200c34c181e1d6b868d3a01890183
David P. Sanders <dpsanders@gmail.com>
2014-11-01 15:11:21 -0600

    Playing with Krawczyk to see if it can be made to work. I think the bisection is where the check for a root should be done, not the K operator

--- src/root_finding/krawczyk.jl
+++ src/root_finding/krawczyk.jl
@@ -4,12 +4,20 @@ const D = differentiate
 function guarded_deriv_midpoint(f, f_prime, x::Interval)
     # avoid 0 derivative
 
+
+
     alpha = 0.5
     m = alpha*x.lo + (1-alpha)*x.hi
 
-    while f_prime(m) == 0
-        alpha /= 2
+    i = 0
+
+    while abs(f(m)) < 1e-10   # don't bisect at/near a root
+        alpha /= 1.1
         m = alpha*x.lo + (1-alpha)*x.hi
+        i += 1
+        if i>10
+            break
+        end
     end
 
     m = Interval(m)
@@ -21,7 +29,7 @@ end
 K(f::Function, f_prime::Function, xx::Interval, m, C) = (
                             # m = Interval(m);
                             # C = Interval(C);
-                            m - C*f(m) + (1. - C*f_prime(xx)) * (xx - m)
+                            m - C*f(m) + (1 - C*f_prime(xx)) * (xx - m)
                             )
 
 K(f::Function, f_prime::Function, x::Interval) =
@@ -40,6 +48,7 @@ function krawczyk_refine(f::Function, f_prime::Function, x::Interval, tolerance=
     i = 0
     while diam(x) > tolerance  # avoid problem with tiny floating-point numbers if 0 is a root
 
+        @show x
         Kx = K(f, f_prime, x) ∩ x
 
         if Kx == x
@@ -72,6 +81,10 @@ function krawczyk(f::Function, f_prime::Function, x::Interval)
 
     if Kx ⊊ x
         return krawczyk_refine(f, f_prime, Kx)
+    end
+
+    if isthin(x)
+        return [(x, :unknown)]
     end
 
     m = mid(x)
