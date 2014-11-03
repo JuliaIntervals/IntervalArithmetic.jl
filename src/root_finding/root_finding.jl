ea55bda7391b1a743b50443e806cded0102994b4
David P. Sanders <dpsanders@gmail.com>
2014-11-03 11:09:28 -0600

    find_roots -> findroots (more Julian)

--- src/root_finding/root_finding.jl
+++ src/root_finding/root_finding.jl
@@ -4,12 +4,12 @@ include("newton.jl")
 include("krawczyk.jl")
 
 
-function find_roots(f::Function, a::Interval, method::Function=newton)
+function findroots(f::Function, a::Interval, method::Function=newton)
     method(f, a)
 end
 
-function find_roots(f::Function, f_prime::Function, a::Interval, method::Function=newton)
+function findroots(f::Function, f_prime::Function, a::Interval, method::Function=newton)
     method(f, f_prime, a)
 end
 
-find_roots(f::Function, a::Real, b::Real) = find_roots(f, Interval(float(a), float(b)))
+findroots(f::Function, a::Real, b::Real) = find_roots(f, Interval(float(a), float(b)))
