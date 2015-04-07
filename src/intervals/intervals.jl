2394e1f1358847e58753458e80ee12008278ef46
David P. Sanders <dpsanders@gmail.com>
2015-04-07 11:00:30 -0500

    Macros now work correctly. Changed conversion so that raw numbers are converted to intervals. Added REQUIRE

--- src/intervals/intervals.jl
+++ src/intervals/intervals.jl
@@ -1,5 +1,6 @@
 include("interval_definition.jl")
 include("interval_macros.jl")
+include("interval_conversion_promotion.jl")
 include("interval_arithmetic.jl")
 include("interval_trig.jl")
 include("interval_functions.jl")
--- test/interval_tests/interval_tests.jl
+++ test/interval_tests/interval_tests.jl
@@ -3,3 +3,5 @@ include("numeric_tests.jl")
 include("trig_tests.jl")
 include("non_BigFloat_tests.jl")
 include("lin_alg_tests.jl")
+include("loop_tests.jl")
+
