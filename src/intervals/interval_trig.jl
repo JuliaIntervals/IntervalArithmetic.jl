34bb353154bcaa9ac01e1ca4f1b96d50a9eeb09b
David P. Sanders <dpsanders@gmail.com>
2015-05-03 13:03:08 -0500

    Rewrote sin to find quartiles by dividing by the interval pi/2. Added alternative_cos that just subtracts the interval pi/2 from its argument

--- src/intervals/interval_trig.jl
+++ src/intervals/interval_trig.jl
@@ -1,80 +1,100 @@
 
 #----- From here on, NEEDS TESTING ------
 
+
+const float_pi_interval = @floatinterval(pi)
+const big_pi_interval = @interval(pi)
+
+get_pi(::Type{BigFloat}) = big_pi_interval
+get_pi(::Type{Float64})  = float_pi_interval
+
+half_pi{T}(::Type{T}) = get_pi(T) / 2
+two_pi{T}(::Type{T})  = get_pi(T) * 2
+
+half_pi(x::FloatingPoint) = half_pi(typeof(x))
+
+
+function find_quartiles(x::FloatingPoint)
+    temp = x / half_pi(x)
+    (itrunc(temp.lo), itrunc(temp.hi))
+end
+
 function sin{T<:Real}(a::Interval{T})
-    piHalf = convert(T, pi) / 2
-    twoPi = convert(T, pi) * 2
-    rangeSin = @round(T, -one(T), one(T))
 
-    # Checking the specific case
-    diam(a) >= twoPi && return rangeSin
+    whole_range = Interval(-one(T), one(T))
 
-    # Limits within 1 full period of sin(x)
-    # Abbreviations
-    loMod2pi = mod(a.lo, twoPi)
-    hiMod2pi = mod(a.hi, twoPi)
-    loQuartile = floor( loMod2pi / piHalf )
-    hiQuartile = floor( hiMod2pi / piHalf )
+    diam(a) >= two_pi(T).lo && return whole_range
 
-    # 20 different cases
-    if loQuartile == hiQuartile # Interval limits in the same quartile
-        loMod2pi > hiMod2pi && return rangeSin
+    lo_quartile = minimum(find_quartiles(a.lo))
+    hi_quartile = maximum(find_quartiles(a.hi))
+
+    lo_quartile = mod(lo_quartile, 4)
+    hi_quartile = mod(hi_quartile, 4)
+
+    # Different cases depending on the two quartiles:
+    if lo_quartile == hi_quartile # Interval limits in the same quartile
+        lo > hi && return whole_range
         return @round(T, sin(a.lo), sin(a.hi))
 
-    elseif loQuartile == 3 && hiQuartile==0
+    elseif lo_quartile==3 && hi_quartile==0
         return @round(T, sin(a.lo), sin(a.hi))
 
-    elseif loQuartile == 1 && hiQuartile==2
+    elseif lo_quartile==1 && hi_quartile==2
         return @round(T, sin(a.hi), sin(a.lo))
 
-    elseif ( loQuartile == 0 || loQuartile==3 ) && ( hiQuartile==1 || hiQuartile==2 )
+    elseif ( lo_quartile == 0 || lo_quartile==3 ) && ( hi_quartile==1 || hi_quartile==2 )
         return @round(T, min(sin(a.lo), sin(a.hi)), one(T))
 
-    elseif ( loQuartile == 1 || loQuartile==2 ) && ( hiQuartile==3 || hiQuartile==0 )
+    elseif ( lo_quartile == 1 || lo_quartile==2 ) && ( hi_quartile==3 || hi_quartile==0 )
         return @round(T, -one(T), max(sin(a.lo), sin(a.hi)))
 
-    elseif ( loQuartile == 0 && hiQuartile==3 ) || ( loQuartile == 2 && hiQuartile==1 )
-        return rangeSin
+    elseif ( lo_quartile == 0 && hi_quartile==3 ) || ( lo_quartile == 2 && hi_quartile==1 )
+        return whole_range
     else
         # This should be never reached!
-        error(string("SOMETHING WENT WRONG in sin.\nThis should have never been reached") )
+        error(string("SOMETHING WENT WRONG in sin with argument $a; this should have never been reached.") )
     end
 end
 
 
+function alternative_cos{T<:Real}(a::Interval{T})
+    sin(a - half_pi(T))
+end
+
+
 function cos{T<:Real}(a::Interval{T})
-    piHalf = convert(T, pi) / 2
-    twoPi = convert(T, pi) * 2
+    half_pi = convert(T, pi) / 2
+    two_pi = convert(T, pi) * 2
     rangeCos = @round(T, -one(T), one(T))
 
     # Checking the specific case
-    diam(a) >= twoPi && return rangeCos
+    diam(a) >= two_pi && return rangeCos
 
     # Limits within 1 full period of sin(x)
     # Abbreviations
-    loMod2pi = mod(a.lo, twoPi)
-    hiMod2pi = mod(a.hi, twoPi)
-    loQuartile = floor( loMod2pi / piHalf )
-    hiQuartile = floor( hiMod2pi / piHalf )
+    lo = mod(a.lo, two_pi)
+    hi = mod(a.hi, two_pi)
+    lo_quartile = floor( lo / half_pi )
+    hi_quartile = floor( hi / half_pi )
 
     # 20 different cases
-    if loQuartile == hiQuartile # Interval limits in the same quartile
-        loMod2pi > hiMod2pi && return rangeCos
+    if lo_quartile == hi_quartile # Interval limits in the same quartile
+        lo > hi && return rangeCos
         return @round(T, cos(a.hi), cos(a.lo))
 
-    elseif loQuartile == 2 && hiQuartile==3
+    elseif lo_quartile == 2 && hi_quartile==3
         return @round(T, cos(a.lo), cos(a.hi))
 
-    elseif loQuartile == 0 && hiQuartile==1
+    elseif lo_quartile == 0 && hi_quartile==1
         return @round(T, cos(a.hi), cos(a.lo))
 
-    elseif ( loQuartile == 2 || loQuartile==3 ) && ( hiQuartile==0 || hiQuartile==1 )
+    elseif ( lo_quartile == 2 || lo_quartile==3 ) && ( hi_quartile==0 || hi_quartile==1 )
         return @round(T, min(cos(a.lo), cos(a.hi)), one(T))
 
-    elseif ( loQuartile == 0 || loQuartile==1 ) && ( hiQuartile==2 || hiQuartile==3 )
+    elseif ( lo_quartile == 0 || lo_quartile==1 ) && ( hi_quartile==2 || hi_quartile==3 )
         return @round(T, -one(T), max(cos(a.lo), cos(a.hi)))
 
-    elseif ( loQuartile == 3 && hiQuartile==2 ) || ( loQuartile == 1 && hiQuartile==0 )
+    elseif ( lo_quartile == 3 && hi_quartile==2 ) || ( lo_quartile == 1 && hi_quartile==0 )
         return rangeCos
     else
         # This should be never reached!
@@ -85,7 +105,7 @@ end
 
 function tan{T}(a::Interval{T})
     bigPi = convert(T, pi)
-    piHalf = bigPi / 2
+    half_pi = bigPi / 2
     rangeTan = Interval{T}( -Inf, Inf )
 
     # Checking the specific case
@@ -95,8 +115,8 @@ function tan{T}(a::Interval{T})
     # some abreviations
     loModpi = mod(a.lo, bigPi)
     hiModpi = mod(a.hi, bigPi)
-    loHalf = floor( loModpi / piHalf )
-    hiHalf = floor( hiModpi / piHalf )
+    loHalf = floor( loModpi / half_pi )
+    hiHalf = floor( hiModpi / half_pi )
 
     I = @round(T, tan(a.lo), tan(a.hi))
 
