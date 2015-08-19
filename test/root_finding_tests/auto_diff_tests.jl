293a372618d755455d7624648eb3cefb9ae0aa44
David P. Sanders <dpsanders@gmail.com>
2015-08-19 07:52:15 -0500

    Added unary plus test for Jet

--- test/root_finding_tests/auto_diff_tests.jl
+++ test/root_finding_tests/auto_diff_tests.jl
@@ -7,24 +7,25 @@ Jet = ValidatedNumerics.Jet
 
 facts("Jet tests") do
     a = Jet(1, 1)
-    
+
     @fact zero(a) == Jet(0, 0) --> true
     @fact one(a)  == Jet(1, 0) --> true
     @fact -a == Jet(-1, -1) --> true
+    @fact +a == 2a - a --> true
 
 end
 
-facts("AutoDiff tests") do	
+facts("AutoDiff tests") do
 
     @fact D(x -> x^2, 3) == 6 --> true
     @fact D(x -> (x+1)/(x-2), 1) == -3.0 --> true
 
     f(x) = sin(2x) - x
-    
+
     for a in (-3, 3, 7, 11)
         @fact D(f, a) == 2*cos(2a) - 1 --> true
         @fact D(tan, a) == 1. / (cos(a)^2) --> true
-        
+
 	if a > 0.
 	    @fact D(log, a) == 1. / a --> true
             @fact D(x -> x^(1//2), a) == 0.5*a^(-0.5) --> true
@@ -38,8 +39,8 @@ facts("AutoDiff tests") do
         @fact D(acos, a) == -1. / sqrt(1 - a^2) --> true
         @fact D(atan, a) == 1. / (1 + a^2) --> true
     end
-    
-    
+
+
 
 end
 
