c2743e8bab4bedfe85326aeffdbace68a888fce6
David Sanders <dpsanders@gmail.com>
2016-02-11 14:05:23 -0500

    Basic support for multidimensional IntervalBox's

--- src/multidim/box.jl
+++ src/multidim/box.jl
@@ -8,9 +8,10 @@ import Base:
 
 export IntervalBox
 
-#typealias IntervalBox{N,T} Vec{N, Interval{T}}  # Vec from FixedSizeArrays
-doc"""A `IntervalBox` is a Cartesian product of an arbitrary number of `Interval`s,
+
+doc"""An `IntervalBox` is a Cartesian product of an arbitrary number of `Interval`s,
 representing an $N$-dimensional rectangular IntervalBox."""
+
 immutable IntervalBox{N, T} <: FixedVector{N, Interval{T}} # uses FixedSizeArrays package
     intervals :: NTuple{N, Interval{T}}
 end
@@ -21,16 +22,6 @@ mid(X::IntervalBox) = [mid(x) for x in X.intervals]
 
 ∩(X::IntervalBox, Y::IntervalBox) = IntervalBox([x ∩ y for (x,y) in zip(X.intervals, Y.intervals)]...)
 isempty(X::IntervalBox) = any(map(isempty, X.intervals))
-
-a = IntervalBox(1..2, 3..4)
-b = IntervalBox(0..2, 3..6)
-
-@assert a ⊆ b
-
-X = IntervalBox(1..2, 3..4)
-Y = IntervalBox(3..4, 3..4)
-
-@assert isempty(X ∩ Y)
 
 
 function Base.show(io::IO, X::IntervalBox)
