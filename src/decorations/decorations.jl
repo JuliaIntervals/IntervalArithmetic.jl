64f130edf1e6fabd3c25fcc5fd5412fa1915620c
David Sanders <dpsanders@gmail.com>
2016-02-26 18:29:14 -0500

    Add a lot of functions to the list

--- src/decorations/decorations.jl
+++ src/decorations/decorations.jl
@@ -4,7 +4,11 @@
 @enum DECORATION ill=0 trv=1 def=2 dac=3 com=4
 # < and min work automatically for enums
 
-
+doc"""
+A `DecoratedInterval` is an interval, together with a *decoration*, i.e.
+a flag that records the status of the interval when thought of as the result
+of a previously executed sequence of functions acting on an initial interval.
+"""
 type DecoratedInterval{T <: AbstractFloat} <: AbstractInterval
     interval::Interval{T}
     decoration::DECORATION
@@ -48,13 +52,44 @@ function Base.sign{T}(xx::DecoratedInterval{T})
 
 end
 
+
+# For domains, cf. table 9.1 on page 28 of the standard
+
 # Functions with restricted domains:
 restricted_functions = Dict(
     :sqrt => [0, ∞],
-    :log => [0, ∞],
 
-    :asin => [-1, 1]
+    :log => [0, ∞],
+    :log2 => [0, ∞],
+    :log10 => [0, ∞],
+
+    :asin => [-1, 1],
+    :acos => [-1, 1],
+
+    :acosh => [1, ∞],
+    :atanh => [-1, 1]
 )
+
+unrestricted_functions =
+    [
+    :-,
+    :exp, :exp2, :exp10,
+    :sin, :cos, :tan,
+    :atan, :atan2,
+    :sinh, :cosh, :tanh,
+    :asinh
+    ]
+
+binary_functions =
+    [
+    :+, :-, :*, :/,
+    :min, :max
+    ]
+
+# three_argument = [:fma]
+# power function must be defined separately and carefully
+
+# Discontinuous like sign:  floor, ceil, trunc, RoundTiesToEven, RoundTiesToAway
 
 # Define functions with restricted domains on DecoratedInterval's:
 for (f, domain) in restricted_functions
@@ -81,7 +116,7 @@ end
 
 # Define unary functions with no domain restrictions
 
-unrestricted_functions = [:exp, :sinh, :cosh]
+
 
 for f in unrestricted_functions
 
@@ -92,7 +127,6 @@ for f in unrestricted_functions
     end
 end
 
-binary_functions = [:+, :-, :*, :/]
 for f in binary_functions
 
     @eval function Base.$(f){T}(xx::DecoratedInterval{T}, yy::DecoratedInterval{T})
