b215e4bb8ee4cf493e48504effd2d0097525f9d7
David P. Sanders <dpsanders@gmail.com>
2017-03-06 16:33:55 -0600

    Add test of f(Z) for functions f of complex intervals Z (#240)
    * Add test of sin(Z) for complex interval Z
    
    * Add a test of e^(i*pi)

--- test/interval_tests/complex.jl
+++ test/interval_tests/complex.jl
@@ -16,3 +16,10 @@ using ValidatedNumerics
     @test a / a == 1
     @test a^2 == -1
 end
+
+@testset "Complex functions" begin
+    Z = (3 ± 1e-7) + (4 ± 1e-7)*im
+    @test sin(Z) == Interval(3.853734949309744, 3.8537411265295507) - Interval(27.016810169394066, 27.016816346613883)*im
+
+    @test exp(-im * Interval(π)) == Interval(-1.0, -0.9999999999999999) - Interval(1.224646799147353e-16, 1.2246467991473532e-16)*im
+end
