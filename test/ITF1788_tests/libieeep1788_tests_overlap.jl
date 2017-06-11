cce95516114c25ffed9fd7b54773f02a506bb845
Luis Benet <benet@fis.unam.mx>
2017-06-11 10:19:06 -0500

    Update itf1788 tests to use Base.Test

--- test/ITF1788_tests/libieeep1788_tests_overlap.jl
+++ test/ITF1788_tests/libieeep1788_tests_overlap.jl
@@ -1,26 +1,26 @@
-#
-# Copyright 2013 - 2015 Marco Nehmeier (nehmeier@informatik.uni-wuerzburg.de)
-# Copyright 2015 Oliver Heimlich (oheim@posteo.de)
-# 
-# Original author: Marco Nehmeier (unit tests in libieeep1788)
-# Converted into portable ITL format by Oliver Heimlich with minor corrections.
-# 
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-# 
-#     http://www.apache.org/licenses/LICENSE-2.0
-# 
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
+#=
+ Copyright 2013 - 2015 Marco Nehmeier (nehmeier@informatik.uni-wuerzburg.de)
+ Copyright 2015 Oliver Heimlich (oheim@posteo.de)
+ 
+ Original author: Marco Nehmeier (unit tests in libieeep1788)
+ Converted into portable ITL format by Oliver Heimlich with minor corrections.
+ 
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+ 
+     http://www.apache.org/licenses/LICENSE-2.0
+ 
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+=#
 #Language imports
 
 #Test library imports
-using FactCheck
+using Base.Test
 
 #Arithmetic library imports
 using IntervalArithmetic
@@ -28,13 +28,15 @@ using IntervalArithmetic
 #Preamble
 setprecision(53)
 setprecision(Interval, Float64)
-# setrounding(Interval, :narrow)
+setrounding(Interval, :tight)
+# Set full format, and show decorations
+@format full
 
-facts("minimal_overlap_test") do
+@testset "minimal_overlap_test" begin
 
 end
 
-facts("minimal_overlap_dec_test") do
+@testset "minimal_overlap_dec_test" begin
 
 end
 # FactCheck.exitstatus()
--- test/ITF1788_tests/libieeep1788_tests_rev.jl
+++ test/ITF1788_tests/libieeep1788_tests_rev.jl
@@ -1,26 +1,26 @@
-#
-# Copyright 2013 - 2015 Marco Nehmeier (nehmeier@informatik.uni-wuerzburg.de)
-# Copyright 2015 Oliver Heimlich (oheim@posteo.de)
-# 
-# Original author: Marco Nehmeier (unit tests in libieeep1788)
-# Converted into portable ITL format by Oliver Heimlich with minor corrections.
-# 
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-# 
-#     http://www.apache.org/licenses/LICENSE-2.0
-# 
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
+#=
+ Copyright 2013 - 2015 Marco Nehmeier (nehmeier@informatik.uni-wuerzburg.de)
+ Copyright 2015 Oliver Heimlich (oheim@posteo.de)
+ 
+ Original author: Marco Nehmeier (unit tests in libieeep1788)
+ Converted into portable ITL format by Oliver Heimlich with minor corrections.
+ 
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+ 
+     http://www.apache.org/licenses/LICENSE-2.0
+ 
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+=#
 #Language imports
 
 #Test library imports
-using FactCheck
+using Base.Test
 
 #Arithmetic library imports
 using IntervalArithmetic
@@ -28,133 +28,135 @@ using IntervalArithmetic
 #Preamble
 setprecision(53)
 setprecision(Interval, Float64)
-# setrounding(Interval, :narrow)
+setrounding(Interval, :tight)
+# Set full format, and show decorations
+@format full
 
-facts("minimal_sqrRev_test") do
-
-end
-
-facts("minimal_sqrRevBin_test") do
+@testset "minimal_sqrRev_test" begin
 
 end
 
-facts("minimal_sqrRev_dec_test") do
+@testset "minimal_sqrRevBin_test" begin
 
 end
 
-facts("minimal_sqrRev_dec_bin_test") do
+@testset "minimal_sqrRev_dec_test" begin
 
 end
 
-facts("minimal_absRev_test") do
+@testset "minimal_sqrRev_dec_bin_test" begin
 
 end
 
-facts("minimal_absRevBin_test") do
+@testset "minimal_absRev_test" begin
 
 end
 
-facts("minimal_absRev_dec_test") do
+@testset "minimal_absRevBin_test" begin
 
 end
 
-facts("minimal_absRev_dec_bin_test") do
+@testset "minimal_absRev_dec_test" begin
 
 end
 
-facts("minimal_pownRev_test") do
+@testset "minimal_absRev_dec_bin_test" begin
 
 end
 
-facts("minimal_pownRevBin_test") do
+@testset "minimal_pownRev_test" begin
 
 end
 
-facts("minimal_pownRev_dec_test") do
+@testset "minimal_pownRevBin_test" begin
 
 end
 
-facts("minimal_pownRev_dec_bin_test") do
+@testset "minimal_pownRev_dec_test" begin
 
 end
 
-facts("minimal_sinRev_test") do
+@testset "minimal_pownRev_dec_bin_test" begin
 
 end
 
-facts("minimal_sinRevBin_test") do
+@testset "minimal_sinRev_test" begin
 
 end
 
-facts("minimal_sinRev_dec_test") do
+@testset "minimal_sinRevBin_test" begin
 
 end
 
-facts("minimal_sinRev_dec_bin_test") do
+@testset "minimal_sinRev_dec_test" begin
 
 end
 
-facts("minimal_cosRev_test") do
+@testset "minimal_sinRev_dec_bin_test" begin
 
 end
 
-facts("minimal_cosRevBin_test") do
+@testset "minimal_cosRev_test" begin
 
 end
 
-facts("minimal_cosRev_dec_test") do
+@testset "minimal_cosRevBin_test" begin
 
 end
 
-facts("minimal_cosRev_dec_bin_test") do
+@testset "minimal_cosRev_dec_test" begin
 
 end
 
-facts("minimal_tanRev_test") do
+@testset "minimal_cosRev_dec_bin_test" begin
 
 end
 
-facts("minimal_tanRevBin_test") do
+@testset "minimal_tanRev_test" begin
 
 end
 
-facts("minimal_tanRev_dec_test") do
+@testset "minimal_tanRevBin_test" begin
 
 end
 
-facts("minimal_tanRev_dec_bin_test") do
+@testset "minimal_tanRev_dec_test" begin
 
 end
 
-facts("minimal_coshRev_test") do
+@testset "minimal_tanRev_dec_bin_test" begin
 
 end
 
-facts("minimal_coshRevBin_test") do
+@testset "minimal_coshRev_test" begin
 
 end
 
-facts("minimal_coshRev_dec_test") do
+@testset "minimal_coshRevBin_test" begin
 
 end
 
-facts("minimal_coshRev_dec_bin_test") do
+@testset "minimal_coshRev_dec_test" begin
 
 end
 
-facts("minimal_mulRev_test") do
+@testset "minimal_coshRev_dec_bin_test" begin
 
 end
 
-facts("minimal_mulRevTen_test") do
+@testset "minimal_mulRev_test" begin
 
 end
 
-facts("minimal_mulRev_dec_test") do
+@testset "minimal_mulRevTen_test" begin
 
 end
 
-facts("minimal_mulRev_dec_ten_test") do
+@testset "minimal_mulRev_dec_test" begin
+
+end
+
+@testset "minimal_mulRev_dec_ten_test" begin
 
 end
 # FactCheck.exitstatus()
