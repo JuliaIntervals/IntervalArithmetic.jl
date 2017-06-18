e36be9c0074f36f6755c2ec896bc4334fc75909c
David P. Sanders <dpsanders@gmail.com>
2017-06-18 17:46:11 -0500

    Add basic benchmarks (#54)
    * Add basic benchmark file
    
    * Add usage instructions
    
    * Add sum benchmarks
    
    * Add some benchmarks of @interval and elementary functions

---
+++ benchmark/benchmarks.jl
@@ -0,0 +1,52 @@
+#= Instructions:
+
+- Pkg.add("PkgBenchmark.jl")
+- using PkgBenchmark
+
+- results = benchmarkpkg("IntervalArithmetic")
+- showall(results)
+
+- results = judge("IntervalArithmetic", "v0.9.1")  # compare current version to that tag
+- showall(results)
+=#
+
+
+using IntervalArithmetic
+
+@benchgroup "Constructors" begin
+    @bench "Interval" Interval(1, 2)
+end
+
+@benchgroup "@interval" begin
+
+    @bench "@interval" @interval(0.1)
+    @bench "pi" @interval(pi)
+    @bench "expression" @interval(sin(0.1) + cos(0.2))
+end
+
+@benchgroup "Arithmetic" begin
+
+    a = Interval(1, 2)
+    b = Interval(3, 4)
+
+    for op in (+, -, *, /)
+        @bench string(op) $(op)($a, $b)
+    end
+end
+
+@benchgroup "Elementary functions" begin
+    a = Interval(1, 2)
+
+    for op in (exp, log, sin, tan)
+        @bench string(op) $(op)($a)
+    end
+end
+
+@benchgroup "Sum" begin
+
+    sum1(N) = sum(Interval(i, i+1) for i in 1:N)
+    sum2(N) = (one = Interval(1.0); sum(one / (i^2) for i in 1:N) )
+
+    @bench "Sum1" sum1(1000)
+    @bench "Sum2" sum2(1000)
+end
