0d07df4d584c3c2a225e6a280deebab2b75d45f5
David P. Sanders <dpsanders@gmail.com>
2018-04-16 19:48:21 -0500

    Revert "Add erf and erfc" (#132)
    * Revert "Fix a - x and add tests (#131)"
    
    This reverts commit 839bb209ce6a20da72bf67e9fa7ab8ad1a5fb143.
    
    * Revert "WIP: Add erf and erfc (#118)"
    
    This reverts commit afb03a367ac1b4db8dcf1b8e1c6b64d1ccfb9de2.

--- src/intervals/functions.jl
+++ src/intervals/functions.jl
@@ -266,24 +266,3 @@ for f in (:log, :log2, :log10, :log1p)
 
         end
 end
-
-for f in (:erf, :erfc)
-    @eval function($f)(x::BigFloat, r::RoundingMode)
-        setrounding(BigFloat, r) do
-            ($f)(x)
-        end
-    end
-
-    @eval ($f)(a::Interval{Float64}) = convert(Interval{Float64}, ($f)(big53(a)))
-
-end
-
-function erf(a::Interval{T}) where T
-    isempty(a) && return a
-    @round( erf(a.lo), erf(a.hi) )
-end
-
-function erfc(a::Interval{T}) where T
-    isempty(a) && return a
-    @round( erfc(a.hi), erfc(a.lo) )
-end
