945180ec128fe3111cb6cffb2b66a9552f85b6fd
David Sanders <dpsanders@gmail.com>
2018-06-07 12:57:08 -0500

    Remove duplicated method

--- src/multidim/intervalbox.jl
+++ src/multidim/intervalbox.jl
@@ -44,7 +44,6 @@ Return a vector of the `mid` of each interval composing the `IntervalBox`.
 
 See `mid(X::Interval, α=0.5)` for more informations.
 """
-mid(X::IntervalBox, α) = mid.(X, α)
 mid(X::IntervalBox) = mid.(X)
 mid(X::IntervalBox, α) = mid.(X, α)
 
