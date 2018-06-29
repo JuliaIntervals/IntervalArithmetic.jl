e6e0d0de4098381336f68e2f3fc414f8e3ad97bb
Marcelo Forets <mforets@gmail.com>
2018-06-29 11:25:47 -0300

    Update benchmarks.jl (#156)
    * Update benchmarks.jl
    
    update benchmarks to use BenchmarkGroup() syntax
    
    * add REQUIRE to benchmarks
    
    * update benchmarks instructions

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -6,45 +6,49 @@
 - results = benchmarkpkg("IntervalArithmetic")
 - showall(results)
 
-- results = judge("IntervalArithmetic", "v0.9.1")  # compare current version to that tag
+To compare the current version with a given release use `judge`:
+
+- results = judge("IntervalArithmetic", tag)
 - showall(results)
+
+To export the benchmark results to a markdown file, do:
+
+- export_markdown("results.md", results)
 =#
 
+using BenchmarkTools, IntervalArithmetic
 
-using IntervalArithmetic
+const SUITE = BenchmarkGroup() # parent BenchmarkGroup to contain our suite
 
-@benchgroup "Constructors" begin
-    @bench "Interval" Interval(1, 2)
-end
+S = SUITE["Constructors"] = BenchmarkGroup()
+S["Interval"] = @benchmarkable Interval(1, 2)
 
-@benchgroup "@interval" begin
+S = SUITE["Intervals"] = BenchmarkGroup()
+S["@interval"] = @benchmarkable @interval(0.1)
+S["pi"] = @benchmarkable @interval(pi)
+S["expression"] = @benchmarkable @interval(sin(0.1) + cos(0.2))
 
-    @bench "@interval" @interval(0.1)
-    @bench "pi" @interval(pi)
-    @bench "expression" @interval(sin(0.1) + cos(0.2))
-end
-
-@benchgroup "Arithmetic" begin
-
+S = SUITE["Arithmetic"] = BenchmarkGroup()
+begin
     a = Interval(1, 2)
     b = Interval(3, 4)
 
     for op in (+, -, *, /)
-        @bench string(op) $(op)($a, $b)
+        S[string(op)] = @benchmarkable $(op)($a, $b)
     end
 end
 
-@benchgroup "Elementary functions" begin
+S = SUITE["Elementary functions"] = BenchmarkGroup()
+begin
     for op in (exp, log, sin, tan)
-        @bench string(op) $(op)($a)
+        S[string(op)] = @benchmarkable $(op)($a)
     end
 end
 
-@benchgroup "Sum" begin
-
+S = SUITE["Sum"] = BenchmarkGroup()
+begin
     sum1(N) = sum(Interval(i, i+1) for i in 1:N)
-    sum2(N) = (one = Interval(1.0); sum(one / (i^2) for i in 1:N) )
-
-    @bench "Sum1" sum1(1000)
-    @bench "Sum2" sum2(1000)
+    sum2(N) = (one = Interval(1.0); sum(one / (i^2) for i in 1:N))
+    S["Sum1"] = @benchmarkable sum1(1000)
+    S["Sum2"] = @benchmarkable sum2(1000)
 end
