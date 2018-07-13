721c7393304e2519c97ab937fe291255d7bbfd8b
David Sanders <dpsanders@gmail.com>
2018-07-13 13:18:53 -0400

    Inline broadcast

--- src/multidim/arithmetic.jl
+++ src/multidim/arithmetic.jl
@@ -21,11 +21,11 @@
 wrap(v::SVector{N,T} where {N,T<:Interval}) = IntervalBox(v)
 wrap(v) = v
 
-Base.size(X::IntervalBox{2,Float64}) = (2,)
+#Base.size(X::IntervalBox{2,Float64}) = (2,)
 #
-broadcasted(f, X::IntervalBox) = wrap(f.(X.v))
-broadcasted(f, X::IntervalBox, Y::IntervalBox) = wrap(f.(X.v, Y.v))
-broadcasted(f, X::IntervalBox, y) = wrap(f.(X.v, y))
+@inline broadcasted(f, X::IntervalBox) = wrap(f.(X.v))
+@inline broadcasted(f, X::IntervalBox, Y::IntervalBox) = wrap(f.(X.v, Y.v))
+@inline broadcasted(f, X::IntervalBox, y) = wrap(f.(X.v, y))
 
 for op in (:+, :-, :∩, :∪, :⊆, :isinterior, :dot, :setdiff, :×)
     @eval $(op)(a::SVector, b::IntervalBox) = $(op)(IntervalBox(a), b)
