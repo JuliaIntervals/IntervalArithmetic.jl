510127e1e725f6364db02870a7e616d637112658
Caroline Wormell <ca.wormell@gmail.com>
2018-08-14 23:11:42 +0100

    Added complex sqrt and some complex set functions

---
+++ src/intervals/complex.jl
@@ -0,0 +1,77 @@
+function ssqs(x::T, y::T,RND::RoundingMode) where T<:AbstractFloat
+    k::Int = 0
+    ρ = +(*(x,x,RND),*(y,y,RND),RND)
+    if !isfinite(ρ) && (isinf(x) || isinf(y))
+        ρ = convert(T, Inf)
+    elseif isinf(ρ) || (ρ==0 && (x!=0 || y!=0)) || ρ<nextfloat(zero(T))/(2*eps(T)^2)
+        m::T = max(abs(x), abs(y))
+        k = m==0 ? m : exponent(m)
+        xk, yk = ldexp(x,-k), ldexp(y,-k)
+        ρ = +(*(xk,xk,RND),*(yk,yk,RND),RND)
+    end
+    ρ, k
+end
+
+function ssqs(x::Interval{T}, y::Interval{T}) where T<:AbstractFloat
+    x = abs(x); y = abs(y)
+    ρl,kl = ssqs(inf(x),inf(y),RoundDown)
+    ρu,ku = ssqs(sup(x),sup(y),RoundUp)
+    ρl, kl, ρu, ku
+end
+
+function sqrt_realpart(x::T, y::T,RND::RoundingMode) where T<:AbstractFloat
+    # @assert x ≥ 0
+    ρ, k = ssqs(x,y,RND)
+    if isfinite(x) ρ = +(ldexp(x,-k),sqrt(ρ,RND),RND) end
+
+    if isodd(k)
+        k = div(k-1,2)
+    else
+        k = div(k,2)-1
+        ρ *= 2
+    end
+    ρ = ldexp(sqrt(ρ,RND),k) #sqrt((abs(z)+abs(x))/2) without over/underflow
+end
+
+function sqrt(z::Complex{Interval{T}}) where T<:AbstractFloat
+    x, y = reim(z)
+
+    (inf(x) < 0 && inf(y) < 0 && sup(y) >= 0) && error("Interval lies across branch cut")
+    x == y == 0 && return Complex(zero(x),y)
+
+    (inf(x) < 0 && sup(x) > 0) && return sqrt((inf(x)..0) + im*y) ∪ sqrt((0..sup(x)) + im*y)
+
+    absx = abs(x)
+
+    absy= abs(y)
+    ρl = sqrt_realpart(inf(absx),inf(absy),RoundDown)
+    ρu = sqrt_realpart(sup(absx),sup(absy),RoundUp)
+
+    ηu = sup(y)
+    if isfinite(ηu)
+        if ηu > 0
+            ηu_re = sqrt_realpart(inf(absx),ηu,RoundDown)
+        else
+            ηu_re = sqrt_realpart(sup(absx),ηu,RoundUp)
+        end
+        ηu = ηu_re > 0 ?  /(ηu,2ηu_re,RoundUp) : zero(T)
+    end
+
+    ηl = inf(y)
+    if ηl > 0
+        ηl_re = sqrt_realpart(sup(absx),ηl,RoundUp)
+    else
+        ηl_re = sqrt_realpart(inf(absx),ηl,RoundDown)
+    end
+    ηl = ηl_re > 0 ? /(ηl,2ηl_re,RoundDown) : zero(T)
+
+    ξ = ρl..ρu
+    η = ηl..ηu
+
+    if mid(x)<0
+        ξ = η*sign(mid(η))
+        η = (ρl..ρu)*sign(mid(y))
+    end
+
+    Complex(ξ,η)
+end
--- test/interval_tests/complex.jl
+++ test/interval_tests/complex.jl
@@ -7,6 +7,8 @@ end
 
 @testset "Complex interval operations" begin
     a = @interval 1im
+    b = @interval 4im + 3
+    
     @test typeof(a)== Complex{IntervalArithmetic.Interval{Float64}}
     @test a ==  Interval(0) + Interval(1)*im
     @test a * a == Interval(-1)
@@ -14,10 +16,16 @@ end
     @test a - a == 0
     @test a / a == 1
     @test a^2 == -1
+
+    @test a ∪ b == (@interval 0 3) + (@interval 1 4)*im
+    @test a ∩ b == ∅
+    @test isdisjoint(a,b) == true
 end
 
 @testset "Complex functions" begin
     Z = (3 ± 1e-7) + (4 ± 1e-7)*im
     @test sin(Z) == complex(sin(real(Z))*cosh(imag(Z)),sinh(imag(Z))*cos(real(Z)))
     @test exp(-im * Interval(π)) == Interval(-1.0, -0.9999999999999999) - Interval(1.224646799147353e-16, 1.2246467991473532e-16)*im
+
+    @test sqrt(Z) == Interval(1.9999999699999995,2.0000000300000007) + Interval(2.0000000300000007,1.0000000300000005)*im
 end
