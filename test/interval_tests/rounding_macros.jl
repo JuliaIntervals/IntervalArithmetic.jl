cea9f90f9fc8b16d55a82e5bc9a17d96278f3365
David Sanders <dpsanders@gmail.com>
2018-11-28 14:16:30 -0600

    Fix tests

--- test/interval_tests/rounding_macros.jl
+++ test/interval_tests/rounding_macros.jl
@@ -1,7 +1,7 @@
 using IntervalArithmetic
 using Test
 
-@test IntervalArithmetic.round_expr(:(a + b), RoundDown) == :($(Expr(:escape, :a)) + $(Expr(:escape, :b)) + $(RoundDown))
+@test IntervalArithmetic.round_expr(:(a + b), RoundDown) == :(($(Expr(:escape, :+)))($(Expr(:escape, :a)), $(Expr(:escape, :b)), RoundingMode{:Down}()))
 
 @test IntervalArithmetic.round_expr(:(sin(a)), RoundUp) ==
-    :( sin( $(Expr(:escape, :a)), $(RoundUp) ) )
+    :(sin($(Expr(:escape, :a)), RoundingMode{:Up}()))
