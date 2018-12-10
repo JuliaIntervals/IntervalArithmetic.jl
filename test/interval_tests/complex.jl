543f7dc62ba6d8feab3409ca02d35a754103ba82
David Sanders <dpsanders@gmail.com>
2018-12-10 10:04:15 -0600

    Add test

--- test/interval_tests/complex.jl
+++ test/interval_tests/complex.jl
@@ -44,3 +44,9 @@ end
     a = -3.1
     @test x ⊆ (x^a)^(1/a)
 end
+
+@testset "abs2 and abs" begin
+    x = (0..0.01) + (0..1)*im
+    @test abs2(x) == Interval(0.0, 1.0001000000000002)
+    @test abs(x) == Interval(0.0, 1.0000499987500626)
+end
