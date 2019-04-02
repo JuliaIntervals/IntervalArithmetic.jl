91f5afcc3aa6ea1dae0ca90db34f31da85adac88
Sheehan Olver <solver@mac.com>
2019-04-02 23:25:20 +0100

    Don't use true/false

--- src/intervals/arithmetic.jl
+++ src/intervals/arithmetic.jl
@@ -357,7 +357,7 @@ end
 """
     signbit(x::Interval)
 
-Returns an interval containing `true` if the value of the sign of any element in `x` is negative, containing `false`
+Returns an interval containing `true` (`1`) if the value of the sign of any element in `x` is negative, containing `false` (`0`)
 if any element in `x` is non-negative, and an empy interval if `x` is empty.
 
 # Examples
