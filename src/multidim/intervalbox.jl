224948b570424bf140d5e068343da00fdf6bceb1
Luis Benet <lbenet@users.noreply.github.com>
2019-04-14 18:51:46 -0500

    Add missing set operations involving `IntervalBox`es, with tests (#262)
    * Add missing set operations involving `IntervalBox`es, with tests
    
    * Use metaprograming and add negative tests

--- src/multidim/intervalbox.jl
+++ src/multidim/intervalbox.jl
@@ -65,8 +65,9 @@ big(X::IntervalBox) = big.(X)
 ## set operations
 
 # TODO: Update to use generator
-⊆(X::IntervalBox{N,T}, Y::IntervalBox{N,T}) where {N,T} =
-    all(X.v .⊆ Y.v)
+for (op, dotop) in ((:⊆, :.⊆), (:⊂, :.⊂), (:⊃, :.⊃))
+    @eval $(op)(X::IntervalBox{N,T}, Y::IntervalBox{N,T}) where {N,T} = all($(dotop)(X, Y))
+end
 
 ∩(X::IntervalBox{N,T}, Y::IntervalBox{N,T}) where {N,T} =
     IntervalBox(X.v .∩ Y.v)
