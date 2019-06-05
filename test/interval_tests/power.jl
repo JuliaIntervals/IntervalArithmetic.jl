98b35feaff79c55bf2003d3839d151338e558aee
yashrajgupta <yashrajgupta98@gmail.com>
2019-06-05 11:01:34 +0530

    add tests

--- test/interval_tests/power.jl
+++ test/interval_tests/power.jl
@@ -16,12 +16,45 @@ setrounding(Interval, :tight)
         @test ^(2 .. 8, 1//3) ⊇  Interval(2^(1//3), 2)
         @test ^(1 .. 9, 1//3) ⊇  Interval(1, 9^(1//3))
         @test ^(2 .. 9, 1//3) ⊇  Interval(2^(1//3), 9^(1//3))
+        @test ^(-1 .. 8, 1//3) == Interval(0, 2)
+        @test ^(-2 .. 8, 1//3) ⊇  Interval(0, 2)
+        @test ^(-1 .. 9, 1//3) ⊇  Interval(0, 9^(1//3))
+        @test ^(-2 .. 9, 1//3) ⊇  Interval(0, 9^(1//3))
         @test ^(1 .. 8, -1//3) == Interval(0.5, 1)
         @test ^(2 .. 8, -1//3) ⊇  Interval(0.5, 2^(-1//3))
         @test ^(1 .. 9, -1//3) ⊇  Interval(9^(-1//3), 1)
         @test ^(2 .. 9, -1//3) ⊇  Interval(9^(-1//3), 2^(-1//3))
+        @test ^(-1 .. 8, -1//3) == Interval(0.5, Inf)
+        @test ^(-2 .. 8, -1//3) ⊇  Interval(0.5, Inf)
+        @test ^(-1 .. 9, -1//3) ⊇  Interval(9^(-1//3), Inf)
+        @test ^(-2 .. 9, -1//3) ⊇  Interval(9^(-1//3), Inf)
         @test ^(-2 .. 4 , 1//2) == Interval(0, 2)
         @test ^(-2 .. 8 , 1//3) == Interval(0, 2)
         @test ^(-8 .. -2 , 1//3) == ∅
         @test ^(-8 .. -2 , 1//2) == ∅
+        @test ^(-8 .. -2 , -1//3) == ∅
+        @test ^(-8 .. -2 , -1//2) == ∅
+        @test ^(∅, 2//3) == ∅
+        @test ^(1 .. 8, 2//3) == Interval(1, 4)
+        @test ^(2 .. 8, 2//3) ⊇  Interval(2^(2//3), 4)
+        @test ^(1 .. 9, 2//3) ⊇  Interval(1, 9^(2//3))
+        @test ^(2 .. 9, 2//3) ⊇  Interval(2^(2//3), 9^(2//3))
+        @test ^(-1 .. 8, 2//3) == Interval(0, 4)
+        @test ^(-2 .. 8, 2//3) ⊇  Interval(0, 4)
+        @test ^(-1 .. 9, 2//3) ⊇  Interval(0, 9^(2//3))
+        @test ^(-2 .. 9, 2//3) ⊇  Interval(0, 9^(2//3))
+        @test ^(1 .. 8, -2//3) == Interval(0.25, 1)
+        @test ^(2 .. 8, -2//3) ⊇  Interval(0.25, 2^(-2//3))
+        @test ^(1 .. 9, -2//3) ⊇  Interval(9^(-2//3), 1)
+        @test ^(2 .. 9, -2//3) ⊇  Interval(9^(-2//3), 2^(-2//3))
+        @test ^(-1 .. 8, -2//3) == Interval(0.25, Inf)
+        @test ^(-2 .. 8, -2//3) ⊇  Interval(0.25, Inf)
+        @test ^(-1 .. 9, -2//3) ⊇  Interval(9^(-2//3), Inf)
+        @test ^(-2 .. 9, -2//3) ⊇  Interval(9^(-2//3), Inf)
+        @test ^(-2 .. 4 , 3//2) == Interval(0, 8)
+        @test ^(-2 .. 8 , 2//3) == Interval(0, 4)
+        @test ^(-8 .. -2 , 2//3) == ∅
+        @test ^(-8 .. -2 , 3//2) == ∅
+        @test ^(-8 .. -2 , -2//3) == ∅
+        @test ^(-8 .. -2 , -3//2) == ∅
 end
