8783142b5f2d18680a57b9cdae1a324c7fcb032e
yashrajgupta <yashrajgupta98@gmail.com>
2019-06-21 17:30:26 +0530

    use faster / method

--- src/intervals/arithmetic.jl
+++ src/intervals/arithmetic.jl
@@ -218,10 +218,10 @@ function extended_div(a::Interval{T}, b::Interval{T}) where T<:Real
     S = typeof(a.lo / b.lo)
     if 0 < b.hi && 0 > b.lo && 0 ∉ a
         if a.hi < 0
-            return (a / interval(0, b.hi), a / interval(b.lo, 0))
+            return (Interval(T(-Inf), /(a.hi, b.hi, RoundUp)), Interval(/(a.hi, b.lo, RoundDown), T(Inf)))
 
         elseif a.lo > 0
-            return (a / interval(b.lo, 0), a / interval(0, b.hi))
+            return (Interval(T(-Inf), /(a.lo, b.lo, RoundUp)), Interval(/(a.lo, b.hi, RoundDown), T(Inf)))
 
         end
     elseif 0 ∈ a && 0 ∈ b
