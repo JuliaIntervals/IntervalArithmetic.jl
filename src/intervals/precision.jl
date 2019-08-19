53e48663e389289d8986f21317bfb0d54d802a4a
Benoît Richard <kolaru@hotmail.com>
2019-08-19 23:08:49 +0200

    Fix typos in precision.jl

--- src/intervals/precision.jl
+++ src/intervals/precision.jl
@@ -1,15 +1,13 @@
 # This file is part of the IntervalArithmetic.jl package; MIT licensed
 
-## Precision:
-
 """
     big53(x::AbstractFlavor{Float64})
 
 Create an equivalent `BigFloat` interval to a given `Float64` interval.
 """
 function big53(a::F) where {F <: AbstractFlavor{Float64}}
-    setprecision(F, 53) do  # precision of Float64
-        atomic(reparametrize(F, BigFloat), a)
+    setprecision(BigFloat, 53) do  # precision of Float64
+        return atomic(reparametrize(F, BigFloat), a)
     end
 end
 
@@ -19,15 +17,15 @@ end
 Convert `x` to `BigFloat`.
 """
 function big53(x::Float64)
-    setprecision(53) do
-        BigFloat(x)
+    setprecision(BigFloat, 53) do
+        return BigFloat(x)
     end
 end
 
-function Base.setrounding(f::Function, ::Type{Rational{T}},
-    rounding_mode::RoundingMode) where T
+function Base.setrounding(f::Function, ::Type{Rational{T}}, 
+                          rounding_mode::RoundingMode) where T
     setrounding(f, float(Rational{T}), rounding_mode)
 end
 
 float(x::F) where {T, F <: AbstractFlavor{T}} = atomic(reparametrize(F, float(T)), x)
-big(x::F) where {F <: AbstractFlavor = atomic(reparametrize(F, BigFloat), x)
+big(x::F) where {F <: AbstractFlavor} = atomic(reparametrize(F, BigFloat), x)
