0dd4367d8892eeede2b28b1cb95d9d6f8289dfc1
krish8484 <akrish136@gmail.com>
2020-02-09 21:28:08 +0530

    Add random samplers

--- test/rand.jl
+++ test/rand.jl
@@ -2,6 +2,8 @@ using IntervalArithmetic
 using Test
 using StaticArrays
 
+
+
 @testset "rand tests" begin
 
     X = 3..4
@@ -12,6 +14,16 @@ using StaticArrays
     Y = IntervalBox(3..4, 5..6)
     for i in 1:100
         @test rand(Y) ∈ Y
+    end
+
+    X = 3..4
+    for i in 1:100
+        @test rand(X,4)[(i%4)+1] ∈ X
+    end
+
+    Y = IntervalBox(3..4, 5..6)
+    for i in 1:100
+        @test rand(Y,2)[(i%2)+1][(i%2)+1] ∈ Y[(i%2)+1]
     end
 
     for T in (Float32, Float64, BigFloat)
