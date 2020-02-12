00d97d72e3f8c800b3059d8b90d31657ade39ec3
Krish Agarwal <akrish136@gmail.com>
2020-02-12 18:18:35 +0530

    Update rand.jl

--- src/rand.jl
+++ src/rand.jl
@@ -9,6 +9,6 @@ Base.eltype(::Type{Interval{Float64}}) = Float64
 Random.rand(rng::AbstractRNG, X::Random.SamplerTrivial{Interval{Float64}}) = Float64(rand(X[])) 
 
 
-Base.eltype(::Type{IntervalBox{2,Float64}}) = SArray{Tuple{2},Float64,1,2}
-Random.rand(rng::AbstractRNG,X::Random.SamplerTrivial{IntervalBox{2,Float64}}) = rand.(X[])
+# Base.eltype(::Type{IntervalBox{2,Float64}}) = SArray{Tuple{2},Float64,1,2}
+# Random.rand(rng::AbstractRNG,X::Random.SamplerTrivial{IntervalBox{2,Float64}}) = rand.(X[])
     
