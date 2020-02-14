7e2e0440ab898a70decc837490766c1d5fa5747e
Krish Agarwal <akrish136@gmail.com>
2020-02-14 09:26:58 +0530

    Update rand.jl

--- src/rand.jl
+++ src/rand.jl
@@ -6,7 +6,7 @@ Base.rand(X::IntervalBox) = rand.(X)
 
 
 Random.gentype(::Type{Interval{T}}) where {T} = T
-Random.rand(rng::AbstractRNG, X::Random.SamplerTrivial{Interval{Float64}}) = Float64(rand(X[])) 
+Random.rand(rng::AbstractRNG, X::Random.SamplerTrivial{Interval{T}}) where {T} = T(rand(X[])) 
 
 
 # Base.eltype(::Type{IntervalBox{2,Float64}}) = SArray{Tuple{2},Float64,1,2}
