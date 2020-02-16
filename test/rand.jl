cb987732830339a160c12c6d6af0ee2c0f0dbf21
Krish Agarwal <akrish136@gmail.com>
2020-02-16 11:11:46 +0530

    Add random samplers (#358)
    * Add random samplers
    
    * Revert "Add random samplers"
    
    This reverts commit 0dd4367d8892eeede2b28b1cb95d9d6f8289dfc1.
    
    * Random samplers added
    
    * Update rand.jl
    
    * Project.toml updated
    
    * Update rand.jl
    
    * Update rand.jl
    
    * Update rand.jl
    
    * Update rand.jl
    
    * Update rand.jl
    
    * Tests for random samplers added
    
    * Update rand.jl

--- test/rand.jl
+++ test/rand.jl
@@ -1,6 +1,7 @@
 using IntervalArithmetic
 using Test
 using StaticArrays
+using Random
 
 @testset "rand tests" begin
 
@@ -14,6 +15,16 @@ using StaticArrays
         @test rand(Y) ∈ Y
     end
 
+    X = 3..4
+    for i in 1:100
+        @test rand(X,4)[(i%4)+1] ∈ X
+    end
+
+#     Y = IntervalBox(3..4, 5..6)
+#     for i in 1:100
+#         @test rand(Y,2)[(i%2)+1][(i%2)+1] ∈ Y[(i%2)+1]
+#     end
+
     for T in (Float32, Float64, BigFloat)
         X = Interval{T}(3, 4)
         @test rand(X) isa T
@@ -22,4 +33,12 @@ using StaticArrays
         @test rand(Y) isa SVector{2,T}
     end
 
+    for T in (Float32, Float64, BigFloat)
+        X = Interval{T}(3, 7)
+        Y = rand(X, 7)
+        @test Y isa Array{T, 1} 
+        for x in Y
+            @test x isa T
+        end
+    end
 end
