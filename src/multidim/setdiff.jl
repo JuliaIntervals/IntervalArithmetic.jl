7b4d377de8d32f05bb791140b306e170a9757883
lucaferranti <49938764+lucaferranti@users.noreply.github.com>
2021-04-18 23:58:43 +0300

    updated version with less allocations

--- src/multidim/setdiff.jl
+++ src/multidim/setdiff.jl
@@ -28,7 +28,7 @@ i.e. the set of `x` that are in `A` but not in `B`.
 Algorithm: Start from the total overlap (in all directions);
 expand each direction in turn.
 """
-function setdiff2(A::IntervalBox{N,T}, B::IntervalBox{N,T}) where {N,T}
+function setdiff1(A::IntervalBox{N,T}, B::IntervalBox{N,T}) where {N,T}
     X = [labelled_setdiff(a, b) for (a, b) in zip(A.v, B.v)]
     # ordered such that the first in each is the excluded interval
 
@@ -55,7 +55,7 @@ function setdiff2(A::IntervalBox{N,T}, B::IntervalBox{N,T}) where {N,T}
 
 end
 
-function setdiff(A::IntervalBox{N,T}, B::IntervalBox{N,T}) where {N,T}
+function setdiff2(A::IntervalBox{N,T}, B::IntervalBox{N,T}) where {N,T}
 
     intersection = A ∩ B
     isempty(intersection) && return [A]
@@ -73,4 +73,37 @@ function setdiff(A::IntervalBox{N,T}, B::IntervalBox{N,T}) where {N,T}
         x[i] = intersection[i]
     end
     filter!(!isempty, result_list)
+end
+
+function setdiff(A::IntervalBox{N,T}, B::IntervalBox{N,T}) where {N,T}
+
+    intersection = A ∩ B
+    isempty(intersection) && return [A]
+
+    result_list = fill(IntervalBox(emptyinterval(T), N), 2*N)
+    offset = 0
+    x = Array(A.v)
+    @inbounds for i = 1:N
+        tmp = _setdiff(A[i], B[i])
+        @inbounds for j = 1:2
+            x[i] = tmp[j]
+            result_list[offset+j] = IntervalBox{N, T}(x)
+        end
+        offset += 2
+        x[i] = intersection[i]
+    end
+    filter!(!isempty, result_list)
+end
+
+function _setdiff(x::Interval{T}, y::Interval{T}) where T
+    intersection = x ∩ y
+
+    isempty(intersection) && return (x, emptyinterval(T))
+    intersection == x && return (emptyinterval(T), emptyinterval(T))  # x is subset of y; setdiff is empty
+
+    x.lo == intersection.lo && return (Interval(intersection.hi, x.hi), emptyinterval(T))
+    x.hi == intersection.hi && return (Interval(x.lo, intersection.lo), emptyinterval(T))
+
+    return (Interval(x.lo, y.lo), Interval(y.hi, x.hi))
+
 end
