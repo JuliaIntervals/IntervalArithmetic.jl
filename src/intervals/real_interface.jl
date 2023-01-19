f9c8bd19b0b47777842339502692468244196fe5
OlivierHnt <38465572+OlivierHnt@users.noreply.github.com>
2023-01-19 13:51:05 -0800

    Revert e04314b7c83f5329f420797f566623c5eccc94b0

--- src/intervals/real_interface.jl
+++ src/intervals/real_interface.jl
@@ -1,16 +1,13 @@
-# This file is part of the IntervalArithmetic.jl package; MIT licensed
-
-#=
-Description:
-
-This file contains the functions that must be defined on `Interval`s so that
-they behave like `Real` in julia.
+#= This file contains the function that must be defined on Intervals
+    so that they behave like Real in julia.
 =#
 
-zero(::F) where {T<:Real, F<:Interval{T}} = F(zero(T))
+real(a::Interval) = a
+
+zero(a::F) where {T<:Real, F<:Interval{T}} = F(zero(T))
 zero(::Type{F}) where {T<:Real, F<:Interval{T}} = F(zero(T))
 
-one(::F) where {T<:Real, F<:Interval{T}} = F(one(T))
+one(a::F) where {T<:Real, F<:Interval{T}} = F(one(T))
 one(::Type{F}) where {T<:Real, F<:Interval{T}} = F(one(T))
 
 typemin(::Type{F}) where {T<:Real, F<:Interval{T}} = F(typemin(T), nextfloat(typemin(T)))
@@ -22,11 +19,11 @@ size(::Interval) = (1,)
 eltype(::F) where {F<:Interval} = F
 
 """
-    numtype(::Interval{T}) where {T}
+    numtype(x::Interval)
 
-Return the type `T` of the bounds of the interval.
+Returns the type of the bounds of the interval.
 
-# Example
+### Example
 
 ```julia
 julia> numtype(1..2)
@@ -41,8 +38,8 @@ eps(::Type{F}) where {T, F<:Interval{T}} = F(eps(T))
 """
     hash(x::Interval, h)
 
-Compute the integer hash code for an interval using the method for composite
-types used in `AutoHashEquals.jl`.
+Computes the integer hash code for an interval using the method for composite
+types used in `AutoHashEquals.jl`
 
 Note that in `IntervalArithmetic.jl`, equality of intervals is given by
 `≛` rather than the `==` operator.
@@ -53,4 +50,3 @@ hash(x::Interval, h::UInt) = hash(sup(x), hash(inf(x), hash(Interval, h)))
 
 # TODO No idea where this comes from and if it is the correct place to put it.
 dist(a::Interval, b::Interval) = max(abs(inf(a)-inf(b)), abs(sup(a)-sup(b)))
-
