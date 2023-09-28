1f1b57cabe2902312488d2e86e262d98db6538e6
Tim Holy <tim.holy@gmail.com>
2023-09-27 23:25:17 -0500

    Make tests more stringent

--- test/autodiff_tests/forwarddiff.jl
+++ test/autodiff_tests/forwarddiff.jl
@@ -6,12 +6,12 @@ using ForwardDiff
     x, w = 2.0, interval(-0.5, 0.5)
     ϕ(t) = sin(x + (1+t)*w)
     ϕ′(t) = cos(x + (1+t)*w) * w
-    ϕ′′(t) = -sin(x + (1+t)*w) * w^2
+    ϕ′′(t) = -sin(x + (1+t)*w) * w * w  # note w * w rather than w^2, since FD goes one order at a time
     ϕ′′′(t) = -cos(x + (1+t)*w) * w^3
     dϕ(t) = ForwardDiff.derivative(ϕ, t)
     ddϕ(t) = ForwardDiff.derivative(dϕ, t)
     dddϕ(t) = ForwardDiff.derivative(ddϕ, t)
-    @test ϕ′(0) ⊆ dϕ(0)
-    @test ϕ′′(0) ⊆ ddϕ(0)
-    @test ϕ′′′(0) ⊆ dddϕ(0)
+    @test ϕ′(0) ≛ dϕ(0)
+    @test ϕ′′(0) ≛ ddϕ(0)
+    @test ϕ′′′(0) ≛ dddϕ(0)
 end
