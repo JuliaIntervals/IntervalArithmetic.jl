6f30309c25fb682820a2c14164c81120df83fb69
agerlach <599421+agerlach@users.noreply.github.com>
2023-10-12 08:52:52 -0400

    Update src/ad.jl
    Co-authored-by: Benoît Richard <kolaru@hotmail.com>

--- src/ad.jl
+++ src/ad.jl
@@ -1,8 +1,8 @@
 function _abs_deriv(x::Interval{T}) where T 
-    if inf(x) == zero(T)
-        return Interval(one(T))
-    elseif sup(x) == zero(T)
-        return -one(T) .. one(T)
+    if inf(x) == 0
+        return Interval{T}(1)
+    elseif sup(x) == 0
+        return Interval{T}(-1, 1)
     else
         return sign(x)
     end
