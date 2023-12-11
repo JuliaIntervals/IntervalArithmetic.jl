7b6020c50a2ff47229a6cfdfcfbaa734851d6e90
OlivierHnt <38465572+OlivierHnt@users.noreply.github.com>
2023-12-11 19:01:45 +0100

    Fix tests

--- test/interval_tests/parsing.jl
+++ test/interval_tests/parsing.jl
@@ -4,7 +4,7 @@
         if T != BigFloat
             @test isequal_interval(parse(BareInterval{T}, "[1e-324, 1e400]"), bareinterval(T, 0, Inf))
         else
-            @test isequal_interval(parse(BareInterval{BigFloat}, "[1e-324, 1e400]"), bareinterval(big"1e-324", big"1e400"))
+            @test isequal_interval(parse(BareInterval{BigFloat}, "[1e-324, 1e400]"), bareinterval(BigFloat("1e-324", RoundDown), BigFloat("1e400", RoundUp)))
         end
         @test isequal_interval(parse(BareInterval{T}, "[2,infinity]"), bareinterval(T, 2, Inf))
         @test isempty_interval(parse(BareInterval{T}, "[foobar]"))
@@ -20,7 +20,7 @@ end
         if T != BigFloat
             @test isequal_interval(parse(Interval{T}, "[1e-324, 1e400]"), interval(T, 0, Inf))
         else
-            @test isequal_interval(parse(Interval{BigFloat}, "[1e-324, 1e400]"), interval(big"1e-324", big"1e400"))
+            @test isequal_interval(parse(Interval{BigFloat}, "[1e-324, 1e400]"), interval(BigFloat("1e-324", RoundDown), BigFloat("1e400", RoundUp)))
         end
         @test isequal_interval(parse(Interval{T}, "[2,infinity]"), interval(T, 2, Inf))
         @test isnai(parse(Interval{T}, "[foobar]"))
