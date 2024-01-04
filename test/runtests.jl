819c694fde7f97033b727302d790d2a7cd0bf613
OlivierHnt <38465572+OlivierHnt@users.noreply.github.com>
2024-01-04 15:16:06 +0100

    Do not generate the ITF1788 test suite automatically

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,24 +4,22 @@ using InteractiveUtils
 
 include("generate_ITF1788.jl")
 
-open("tests_warning.log", "w") do io
-    redirect_stderr(io) do
-        # interval tests
-        for f ∈ readdir("interval_tests"; join = true)
-            @testset "$f" begin
-                include(f)
-            end
-        end
-        # ITF1788 tests
-        for f ∈ readdir("itl")
-            if !occursin("LICENSE", f)
-                generate(f)
-            end
-        end
-        for f ∈ readdir("ITF1788_tests"; join = true)
-            @testset "$f" begin
-                include(f)
-            end
-        end
+# interval tests
+for f ∈ readdir("interval_tests"; join = true)
+    @testset "$f" begin
+        include(f)
+    end
+end
+
+# ITF1788 tests
+# these tests were generated using:
+# for f ∈ readdir("itl")
+#     if !occursin("LICENSE", f)
+#         generate(f)
+#     end
+# end
+for f ∈ readdir("ITF1788_tests"; join = true)
+    @testset "$f" begin
+        include(f)
     end
 end
