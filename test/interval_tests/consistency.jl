073c5054495fa6ed8d64cfe44b952b9380025104
OlivierHnt <38465572+OlivierHnt@users.noreply.github.com>
2024-02-01 12:02:03 +0100

    Fix tests

--- test/interval_tests/consistency.jl
+++ test/interval_tests/consistency.jl
@@ -344,24 +344,24 @@
         @test x != y
         @test x < y
         @test isfinite(x)
-        @test isnan(x)
+        @test !isnan(x)
         @test isinteger(x)
         @test x == 1
         @test isone(x)
         @test !iszero(x)
 
-        @test_throws ArgumentError isdisjoint(x, y)
-        @test_throws ArgumentError issubset(x, y)
-        @test_throws ArgumentError issetequal(x, y)
-        @test_throws ArgumentError x ∈ y
-        @test_throws ArgumentError isempty(x)
+        @test_throws ArgumentError !isdisjoint(x, y)
+        @test_throws ArgumentError !issubset(x, y)
+        @test_throws ArgumentError !issetequal(x, y)
+        @test_throws ArgumentError !(x ∈ y)
+        @test_throws ArgumentError !isempty(x)
 
         x, y = interval(2, 3), interval(1)
 
         @test_throws ArgumentError x != y
         @test_throws ArgumentError x < y
         @test_throws ArgumentError isfinite(x)
-        @test_throws ArgumentError isnan(x)
+        @test_throws ArgumentError !isnan(x)
         @test_throws ArgumentError isinteger(x)
         @test_throws ArgumentError x == 1
         @test_throws ArgumentError isone(x)
