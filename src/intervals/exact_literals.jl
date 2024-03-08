da1f8a112875d317ab34301e111df24b92a0f96c
Benoît Richard <kolaru@hotmail.com>
2024-03-08 17:00:10 +0100

    More renaming

--- src/intervals/exact_literals.jl
+++ src/intervals/exact_literals.jl
@@ -1,5 +1,5 @@
-struct ExactReal{T <: Real} <: Real
-    number::T
+struct ExactReal{T <: Real, F} <: Real
+    value::T
 end
 
 # Promotion to Interval
@@ -22,20 +22,20 @@ Base.promote_rule(::Type{ExactReal{T}}, ::Type{S}) where {T<:Real,S<:Real} =
     promote_type(T, S)
 
 Base.convert(::Type{T}, x::ExactReal) where {T<:Real} =
-    convert(T, x.number)
+    convert(T, x.value)
 
 # Remove the fallback for Real number that go to the constructor T(x) and
 # would allow to create ExactReal from other sources than literals.
 Base.convert(::Type{<:ExactReal}, x::Real) =
     throw(ArgumentError("converting to ExactReal is not allowed"))
 
-interval(::Type{T}, x::ExactReal) where {T<:NumTypes} = interval(T, x.number)
-interval(x::ExactReal) = interval(x.number)
+interval(::Type{T}, x::ExactReal) where {T<:NumTypes} = interval(T, x.value)
+interval(x::ExactReal) = interval(x.value)
 
 Base.string(x::ExactReal{T}) where {T<:AbstractFloat} =
-    Base.Ryu.writefixed(x.number, 2000, false, false, false, UInt8('.'), true)
+    Base.Ryu.writefixed(x.value, 2000, false, false, false, UInt8('.'), true)
 
-Base.string(x::ExactReal) = string(x.number)
+Base.string(x::ExactReal) = string(x.value)
 
 Base.show(io::IO, ::MIME"text/plain", x::ExactReal{T}) where {T<:AbstractFloat} =
     print(io, "ExactReal{$T}($(string(x)))")
