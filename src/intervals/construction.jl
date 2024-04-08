e0fa63455245de4995a8c4b6bd62967bfb4efba2
OlivierHnt <38465572+OlivierHnt@users.noreply.github.com>
2024-04-08 11:42:46 +0200

    Re-implement single argument `@interval`

--- src/intervals/construction.jl
+++ src/intervals/construction.jl
@@ -627,6 +627,7 @@ end
 # macro
 
 """
+    @interval(expr)
     @interval(T, expr)
     @interval(T, expr1, expr2)
 
@@ -638,13 +639,20 @@ constructor [`atomic`](@ref).
 ```jldoctest
 julia> setdisplay(:full);
 
-julia> @macroexpand @interval Float64 sin(1)
+julia> @macroexpand @interval sin(1) # Float64 is the default bound type
 :(sin(IntervalArithmetic.atomic(Float64, 1)))
+
+julia> @macroexpand @interval Float32 sin(1)
+:(sin(IntervalArithmetic.atomic(Float32, 1)))
 
 julia> @interval Float64 sin(1) exp(1)
 Interval{Float64}(0.8414709848078965, 2.7182818284590455, com)
 ```
 """
+macro interval(expr)
+    return _wrap_interval(default_numtype(), expr)
+end
+
 macro interval(T, expr)
     return _wrap_interval(T, expr)
 end
