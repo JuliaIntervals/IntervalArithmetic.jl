f043cfdd19d835f5eadd14183ca62d765f8a8978
OlivierHnt <38465572+OlivierHnt@users.noreply.github.com>
2024-05-31 21:59:31 +0200

    Minor code clean up

--- ext/IntervalArithmeticForwardDiffExt.jl
+++ ext/IntervalArithmeticForwardDiffExt.jl
@@ -3,75 +3,71 @@ module IntervalArithmeticForwardDiffExt
 using IntervalArithmetic, ForwardDiff
 using ForwardDiff: Dual, ≺, value, partials
 
-function isconstant_interval(x)
-    all(isthinzero.(values(partials(x))))
-end
+#
 
-function Base.:(^)(x::Dual{Txy, <:Interval}, y::Dual{Txy, <:Interval}) where Txy
+function Base.:(^)(x::Dual{Txy,<:Interval}, y::Dual{Txy,<:Interval}) where {Txy}
     vx, vy = value(x), value(y)
     expv = vx^vy
     powval = vy * vx^(vy - interval(1))
-    if isconstant_interval(y)
-        logval = one(expv)
+    if all(isthinzero, values(partials(y)))
+        return Dual{Txy}(expv, ForwardDiff._mul_partials(partials(x), partials(y), powval, one(expv)))
     elseif isthinzero(vx) && inf(vy) > 0
-        logval = zero(vx)
+        return Dual{Txy}(expv, ForwardDiff._mul_partials(partials(x), partials(y), powval, zero(vx)))
     else
-        logval = expv * log(vx)
+        return Dual{Txy}(expv, ForwardDiff._mul_partials(partials(x), partials(y), powval, expv * log(vx)))
     end
-    new_partials = ForwardDiff._mul_partials(partials(x), partials(y), powval, logval)
-    return Dual{Txy}(expv, new_partials)
 end
 
-function Base.:(^)(x::Dual{Tx, <:Interval}, y::Dual{Ty, <:Interval}) where {Tx, Ty}
-    if Ty ≺ Tx 
+function Base.:(^)(x::Dual{Tx,<:Interval}, y::Dual{Ty,<:Interval}) where {Tx,Ty}
+    if Ty ≺ Tx
         return x^value(y)
     else
         return value(x)^y
     end
 end
 
-function Base.:(^)(x::Dual{Tx, <:Interval}, y::Interval) where Tx
+function Base.:(^)(x::Dual{Tx,<:Interval}, y::Interval) where {Tx}
     v = value(x)
     expv = v^y
-    if isthinzero(y) || isconstant_interval(x)
-        new_partials = zero(partials(x))
+    if isthinzero(y) || all(isthinzero, values(partials(x)))
+        return Dual{Tx}(expv, zero(partials(x)))
     else
-        new_partials = partials(x) * y * v^(y - interval(1))
+        return Dual{Tx}(expv, partials(x) * y * v^(y - interval(1)))
     end
-    return Dual{Tx}(expv, new_partials)
 end
 
-function Base.:(^)(x::Interval, y::Dual{Ty, <:Interval}) where Ty
+function Base.:(^)(x::Interval, y::Dual{Ty,<:Interval}) where {Ty}
     v = value(y)
     expv = x^v
-    deriv = (isthinzero(x) && inf(v) > 0) ? zero(expv) : expv*log(x)
-    return Dual{Ty}(expv, deriv * partials(y))
+    if isthinzero(x) && inf(v) > 0
+        return Dual{Ty}(expv, zero(expv) * partials(y))
+    else
+        return Dual{Ty}(expv, expv * log(x) * partials(y))
+    end
 end
 
-function Base.:(^)(x::Dual{<:Any, I}, y::ExactReal) where I<:Interval
-    return x^convert(I, y)
-end
+Base.:(^)(x::Dual{<:Any,I}, y::ExactReal) where {I<:Interval} = x^convert(I, y)
 
-function Base.:(^)(x::ExactReal, y::Dual{<:Any, I}) where I<:Interval
-    return convert(I, x)^y
-end
+Base.:(^)(x::ExactReal, y::Dual{<:Any,I}) where {I<:Interval} = convert(I, x)^y
 
-function Base.:(^)(x::Dual{<:Tx}, y::ExactReal) where Tx
+function Base.:(^)(x::Dual{Tx}, y::ExactReal) where {Tx}
     v = value(x)
     expv = v^y
-    if iszero(y.value) || all(iszero.(values(partials(x))))
-        new_partials = zero(partials(x))
+    if iszero(y.value) || all(iszero, values(partials(x)))
+        return Dual{Tx}(expv, zero(partials(x)))
     else
-        new_partials = partials(x) * y * v^(y - 1)
+        return Dual{Tx}(expv, partials(x) * y * v^(y - 1))
     end
-    return Dual{Tx}(expv, new_partials)
 end
 
-function Base.:(^)(x::ExactReal, y::Dual{<:Ty}) where Ty
+function Base.:(^)(x::ExactReal, y::Dual{<:Ty}) where {Ty}
     v = value(y)
     expv = x^v
-    deriv = (iszero(x) && inf(v) > 0) ? zero(expv) : expv*log(x)
-    return Dual{Ty}(expv, deriv * partials(y))
+    if iszero(x) && inf(v) > 0
+        return Dual{Ty}(expv, zero(expv) * partials(y))
+    else
+        return Dual{Ty}(expv, expv * log(x) * partials(y))
+    end
 end
 
 end
