5078432b3eaa38aa0f174a2af4eec582da23a7fb
Benoît Richard <kolaru@hotmail.com>
2025-02-09 23:16:17 +0100

    Doc doc doc

--- src/piecewise.jl
+++ src/piecewise.jl
@@ -1,3 +1,14 @@
+"""
+    Domain{LeftBound, RightBound}(lo, hi)
+
+The domain of a function.
+
+`LeftBound` and `RightBound` must be symbols and are either `:closed` or
+`:open` determining if the corresponding endpoint is (respectively)
+included or not in the domain.
+
+If `hi > lo`, the domain is considered to be empty.
+"""
 struct Domain{L, R, T, S}
     lo::T
     hi::S
@@ -83,7 +94,30 @@ function Base.isempty(domain::Domain)
     return lo > hi
 end
 
+"""
+    Constant(value)
 
+A constant function compatible with interval arithmetic.
+
+Return an interval containing only the value for an interval input,
+and the value directly otherwise.
+
+```jldoctest
+julia> c = Constant(1.2)
+Constant{Float64}(1.2)
+
+julia> c(22.2)
+1.2
+
+julia> c(interval(0, 1.3))
+[1.19999, 1.20001]_com
+```
+
+Note that this is not equivalent to `Returns(value)` from base,
+which always outputs `value`, even for an interval input.
+This can shortcircuit the propagation of intervals in the computation
+and lose the associated guarantee of correctness.
+"""
 struct Constant{T}
     value::T
 end
@@ -91,6 +125,55 @@ end
 (constant::Constant)(::Any) = constant.value
 (constant::Constant)(::Interval) = interval(constant.value)
 
+"""
+    Piecewise(pairs... ; continuity = fill(-1, length(pairs) - 1))
+
+A function defined by pieces (each associating a domain to a function).
+Support both intervals and standard numbers.
+
+```jldoctest
+julia> myabs = Piecewise(
+          Domain{:open, :closed}(-Inf, 0) => x -> -x,
+          Domain{:open, :open}(0, Inf) => identity
+       )
+Piecewise function with 2 pieces:
+  (-Inf, 0] -> var"#119#120"()
+  (0, Inf) -> identity
+
+julia> myabs(-22.3)
+22.3
+
+julia> myabs(interval(-5, 5))
+[0.0, 5.0]_def
+```
+
+For constant pieces, it is recommended to use `Constant`
+for full compatibility with intervals.
+
+The domains must be specified in increasing order and must not overlap.
+
+The `continuity` optional argument takes a vector of `N - 1` integers
+(where  `N` is the number of domains)
+determining how the piecewise function behaves at the endpoints between
+the subdomains.
+The possibility are:
+- `-1` : the function is discontinuous between the domains.
+- `0` : the function is continuous but not differentiable between the domains.
+- `n > 0` : the function is `n` times continuously differentiable between the
+    domains. This only matter when using `ForwardDiff` to compute derivative
+    of the function.
+
+This information is used to determine the decoration of intervals that
+covers the endpoint of several domains.
+
+If an input interval goes outside the domain of definition of the piecewise
+function, the output will always have the trivial (`trv`) decoration.
+For standard number, it throws a `DomainError`.
+
+The piecewise function can have a gap between two pieces.
+In this case, the `continuity` optional argument is ignored,
+and interval spanning over the gap always as the `trv` decoration.
+"""
 struct Piecewise{N, D<:Tuple, F<:Tuple, S<:Tuple}
     domains::D
     fs::F
