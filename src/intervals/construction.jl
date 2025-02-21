5599ee9a19546feffc264ba89b7f66f9db9174c8
OlivierHnt <38465572+OlivierHnt@users.noreply.github.com>
2025-02-22 00:29:08 +0100

    Remove `@interval T a b`

--- src/intervals/construction.jl
+++ src/intervals/construction.jl
@@ -638,7 +638,6 @@ end
 """
     @interval(expr)
     @interval(T, expr)
-    @interval(T, expr1, expr2)
 
 Walk through an expression and wrap each argument of functions with the internal
 constructor [`atomic`](@ref).
@@ -646,16 +645,11 @@ constructor [`atomic`](@ref).
 # Examples
 
 ```jldoctest
-julia> setdisplay(:full);
-
 julia> @macroexpand @interval sin(1) # Float64 is the default bound type
 :(sin(IntervalArithmetic.atomic(Float64, 1)))
 
 julia> @macroexpand @interval Float32 sin(1)
 :(sin(IntervalArithmetic.atomic(Float32, 1)))
-
-julia> @interval Float64 sin(1) exp(1)
-Interval{Float64}(0.8414709848078965, 2.7182818284590455, com)
 ```
 """
 macro interval(expr)
@@ -664,12 +658,6 @@ end
 
 macro interval(T, expr)
     return _wrap_interval(T, expr)
-end
-
-macro interval(T, expr1, expr2)
-    x = _wrap_interval(T, expr1)
-    y = _wrap_interval(T, expr2)
-    return :(interval($(esc(T)), $x, $y))
 end
 
 _wrap_interval(T, x) = :(atomic($(esc(T)), $x))
