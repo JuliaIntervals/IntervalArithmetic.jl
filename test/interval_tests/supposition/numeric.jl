cecb913653f6d8c655c4992a7a078663ca984e0b
bvrb <bvrb@posteo.org>
2025-10-17 15:19:34 +0200

    Update Supposition test for degenerate interval construction - decrease number of random samples - add test for floats - switch from Int8 to Int - change equality check

--- test/interval_tests/supposition/numeric.jl
+++ test/interval_tests/supposition/numeric.jl
@@ -2,20 +2,29 @@ using Test
 using IntervalArithmetic
 using Supposition, Supposition.Data
 
+# Define properties to be checked
+function degenerate_interval(a)
+    x = interval(a)
+    y = interval(a, a)
+    x === y
+end
+
+@testset "Float tests" begin
+    # Define number generators
+    floatgen = Data.Floats()
+
+    # Check properties
+    @check max_examples=1000 degenerate_interval(floatgen)
+end
+
 @testset "Rational tests" begin
     # Define number generators
-    #intgen = Data.Integers(typemin(Int)+1,typemax(Int)) # Don't allow den==typemin(Int) to avoid overflow
-    intgen = Data.Integers(typemin(Int8)+Int8(1),typemax(Int8)) # Don't allow den==typemin(Int8) to avoid overflow
+    intgen = Data.Integers(typemin(Int)+1,typemax(Int)) # Don't allow typemin(Int) to avoid overflow
     rationalgen = @composed function generate_rational(num=intgen, den=intgen)
         assume!(!(iszero(num) && iszero(den)))
         return num // den
     end
 
     # Check properties
-    @check function degenerate_interval(r=rationalgen)
-        x = interval(r)
-        y = interval(r, r)
-        assume!(!any(decoration.((x,y)) .== ill)) # Exclude ill-formed intervals
-        isequal_interval(x, y)
-    end
+    @check max_examples = 1000 degenerate_interval(rationalgen)
 end
