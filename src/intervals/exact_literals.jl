6a19b6ad0ca0b0ec1ca001653933796fcb0c5035
OlivierHnt <38465572+OlivierHnt@users.noreply.github.com>
2026-04-07 18:04:58 +0800

    Negative of default floating-point numbers do not overflow

--- src/intervals/exact_literals.jl
+++ src/intervals/exact_literals.jl
@@ -172,7 +172,8 @@ _str_repr(x::ExactReal{T}) where {T<:AbstractFloat} =
 
 # always exact
 
-Base.:-(x::ExactReal) = exact(-x.value)
+Base.:-(x::ExactReal{<:Union{Float16,Float32,Float64,BigFloat}}) = exact(-x.value)
+Base.:-(x::ExactReal{<:Rational}) = exact(-x.value)
 Base.:-(x::ExactReal{<:Integer}) = exact(Base.checked_neg(x.value))
 
 Base.:*(x::ExactReal, y::ExactReal{Bool}) = exact(x.value * y.value)
@@ -224,6 +225,7 @@ _exact_div(x::Union{Rational,Integer}, y::Union{Rational,Integer}) = x // y
 
 # in general, exactness is lost
 
+Base.:-(x::ExactReal) = -x.value
 Base.:+(x::ExactReal, y::ExactReal) = x.value + y.value
 Base.:-(x::ExactReal, y::ExactReal) = x.value - y.value
 Base.:*(x::ExactReal, y::ExactReal) = x.value * y.value
