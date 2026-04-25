454644d54c1b2ad068d0250df58557e5b7d6d841
Olivier Hénot <38465572+OlivierHnt@users.noreply.github.com>
2026-04-25 17:21:46 +0800

    Remove broken test for Julia v1.14 and above

--- test/interval_tests/IntervalArithmeticIrrationalConstantsExt.jl
+++ test/interval_tests/IntervalArithmeticIrrationalConstantsExt.jl
@@ -19,7 +19,7 @@ import IrrationalConstants
         end
 
         for T ∈ InteractiveUtils.subtypes(Signed)
-            @test in_interval(irr, interval(Rational{T}, irr)) broken=(irr == IrrationalConstants.invsqrt2π && T == Int8)
+            @test in_interval(irr, interval(Rational{T}, irr)) broken=(irr == IrrationalConstants.invsqrt2π && T == Int8 && VERSION ≤ v"13")
         end
 
         # Negative irrationals obviously lack unsigned representations
